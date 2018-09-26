9db38ec5dacfb3e455294ddc8adf723a9f5cf303
Mariano Botta <marianoabotta@gmail.com>
2018-09-26 15:02:13 -0300

    initial commit

---
+++ cypress/support/commands.js
@@ -0,0 +1,51 @@
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add("login", (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add("dismiss", { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This is will overwrite an existing command --
+// Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
+Cypress.Commands.add("login", (usuario, password) => {
+    let token;
+    return cy.request('POST', 'http://localhost:3002/api/auth/login', { usuario, password }).then((response) => {
+        token = response.body.token;
+        return response = cy.request({
+            url: 'http://localhost:3002/api/auth/organizaciones',
+            method: 'GET',
+            headers: {
+                Authorization: 'JWT ' + token
+            },
+        }).then((response) => {
+            let org = response.body[0];
+            return response = cy.request({
+                url: 'http://localhost:3002/api/auth/organizaciones',
+                method: 'POST',
+                headers: {
+                    Authorization: 'JWT ' + token
+                },
+                body: { organizacion: org.id }
+            }).then((response) => {
+                return response.body.token;
+            });
+        });
+    });
+})
