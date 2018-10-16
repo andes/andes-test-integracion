199eccf7df795137ce3f016315811df2c35da378
marcosantarelli <marcosavino19@gmail.com>
2018-10-16 11:32:07 -0300

    iniciar prestacion

---
+++ cypress/integration/iniciarPrestacion.spec..js
@@ -0,0 +1,60 @@
+
+
+/// <reference types="Cypress" />
+
+context('Aliasing', () => {
+    let token
+    before(() => {
+        cy.login('38906735', 'asd').then(t => {
+            token = t;
+        })
+    })
+
+    beforeEach(() => {
+        cy.viewport(1280, 720)
+        cy.visit(Cypress.env('BASE_URL') + '/rup', {
+            onBeforeLoad: (win) => {
+                win.sessionStorage.setItem('jwt', token);
+            }
+        });
+    })
+
+    it('iniciar prestacion', () => {
+        cy.get('.mdi-asterisk').click();
+        cy.get('plex-button[label="INICIAR PRESTACIÓN"]').first().click();
+        cy.get('button').contains('CONFIRMAR').click()
+        cy.get('plex-text[name="searchTerm"] input').first().type('fiebre')
+        cy.wait(1000)
+        cy.get('.btn-hallazgo').click();
+        cy.get('.mdi-plus').first().click();
+        cy.get('.ql-editor p').first().type('fiebre');
+        cy.get('plex-button').contains('Guardar Consulta de medicina general').click();
+        cy.wait(3000)
+        cy.get('plex-button').contains('Validar Consulta de medicina general').first().click();
+        cy.get('button').contains('CONFIRMAR').click()
+
+        
+        
+        
+
+
+        
+
+    })
+
+    // it('Mock api', () => {
+    //     cy.server();
+    //     cy.route('**/api/core/mpi/pacientes**', [
+    //         { "documento":"34934522", "estado":"temporal","nombre":"MARIANO","apellido":"BOTTA", "sexo":"masculino", "genero":"masculino", "fechaNacimiento":"2018-05-01T10:41:52.686Z",   "claveBlocking":["VTMRN","VT","MRN","133696","133"],   "edadReal":{"unidad":"Días","valor":8},"edad":0,"nombreCompleto":"MARIANO BOTTA","id":"5aec38faca43621f0823c85e"},
+    //         { "documento":"30000000", "estado":"validado","nombre":"MARIANO ANDES","apellido":"BOTTA", "sexo":"masculino", "genero":"masculino", "fechaNacimiento":"2018-05-01T10:41:52.686Z",   "claveBlocking":["VTMRN","VT","MRN","133696","133"],   "edadReal":{"unidad":"Días","valor":8},"edad":0,"nombreCompleto":"MARIANO BOTTA","id":"5aec38faca43621f0823c85e"}
+    //     ]).as('elastic');
+    //     cy.get('plex-text input[type=text]').first().type('botta').should('have.value', 'botta');
+    //     cy.wait('@elastic');
+
+    //     cy.get('table').find('tbody').find('tr').should('have.length', 2);
+
+    // });
+
+
+
+})
