db65f8ee7b47234f3c71efe08c52d997f9951317
LucasGonzalez <lucasgonzalezm3@gmail.com>
2018-11-29 09:44:44 -0300

    Testea la auditoría de MPI

---
+++ cypress/integration/auditoria/auditoria.spec.js
@@ -0,0 +1,70 @@
+/// <reference types="Cypress" />
+
+context('Aliasing', () => {
+    let token
+    before(() => {
+        cy.login('36429722', 'pkwt9163').then(t => {
+            token = t;
+        })
+    })
+    beforeEach(() => {
+        cy.viewport(1280, 720)
+
+        cy.visit(Cypress.env('BASE_URL') + '/apps/mpi/auditoria', {
+            onBeforeLoad: (win) => {
+                win.sessionStorage.setItem('jwt', token);
+            }
+        });
+    })
+    it('vincular dos pacientes validados', () => {
+        cy.server();
+        cy.get('paciente-buscar').get('plex-text input').first().type('39130233');
+        cy.wait(2000); // espero 2s para que se carguen los resultados en la tabla
+        cy.get('tbody tr').first().find('span').should('have.class', 'badge badge-success').should('contain', 'Validado').first().click(); // selecciono el primer resultado de la tabla
+        cy.get('plex-layout-footer plex-button[label="Vincular"]').click();
+        cy.wait(4000);
+        cy.get('paciente-buscar').get('plex-text input').first().type('39404080');
+        cy.wait(2000); // espero 2s para que se carguen los resultados en la tabla
+        cy.get('tbody tr').first().find('span').should('have.class', 'badge badge-success').should('contain', 'Validado').first().click(); // valido que la persona buscada esté validada y la selecciono
+
+        cy.get('plex-button[label="Vincular"]').click();
+        cy.get('button').contains('CONFIRMAR').click();
+        cy.get('tbody tr').first().find('span').should('have.class', 'badge badge-success').should('contain', 'Activo'); // valido que la persona buscada esté validada y la selecciono
+        cy.get('plex-button[label="Desactivar"]').should('have.attr', 'type', 'warning');
+    });
+
+    it('vincular un paciente temporal con uno temporal', () => {
+        cy.server();
+        cy.get('paciente-buscar').get('plex-text input').first().type('36606632'); // paciente temporal
+        cy.wait(2000); // espero 2s para que se carguen los resultados en la tabla
+        cy.get('tbody tr').first().find('span').should('have.class', 'badge badge-warning').should('contain', 'Temporal').first().click(); // selecciono el primer resultado de la tabla
+        cy.get('plex-layout-footer plex-button[label="Vincular"]').click();
+        cy.wait(2000);
+        cy.get('legend').should('have.text', 'Listado de pacientes candidatos'); // debe cambiar de componente para hacer la búsqueda del paciente a vincular con el primero
+        cy.get('paciente-buscar').get('plex-text input').first().type('nicolas vicentelo'); // paciente temporal
+        cy.wait(2000); // espero 2s para que se carguen los resultados en la tabla
+        cy.get('tbody tr').first().find('span').should('have.class', 'badge badge-warning').should('contain', 'Temporal').first().click(); // valido que la persona buscada esté validada y la selecciono
+
+        cy.get('plex-button[label="Vincular"]').click();
+        cy.get('button').contains('CONFIRMAR').click();
+        cy.get('tbody tr').first().find('span').should('have.class', 'badge badge-success').should('contain', 'Activo'); // valido que la persona buscada esté validada y la selecciono
+        cy.get('plex-button[label="Desactivar"]').should('have.attr', 'type', 'warning');
+    });
+
+    it('vincular un paciente validado con uno temporal', () => {
+        cy.server();
+        cy.get('paciente-buscar').get('plex-text input').first().type('31965283'); // paciente validado
+        cy.wait(2000); // espero 2s para que se carguen los resultados en la tabla
+        cy.get('tbody tr').first().find('span').should('have.class', 'badge badge-success').should('contain', 'Validado').first().click(); // selecciono el primer resultado de la tabla
+        cy.get('plex-layout-footer plex-button[label="Vincular"]').click();
+        cy.wait(2000);
+        cy.get('legend').should('have.text', 'Listado de pacientes candidatos'); // debe cambiar de componente para hacer la búsqueda del paciente a vincular con el primero
+        cy.get('paciente-buscar').get('plex-text input').first().type('35468716'); // paciente temporal
+        cy.wait(2000); // espero 2s para que se carguen los resultados en la tabla
+        cy.get('tbody tr').first().find('span').should('have.class', 'badge badge-warning').should('contain', 'Temporal').first().click(); // valido que la persona buscada esté validada y la selecciono
+        cy.get('plex-button[label="Vincular"]').click();
+        cy.get('button').contains('CONFIRMAR').click();
+        cy.get('tbody tr').first().find('span').should('have.class', 'badge badge-success').should('contain', 'Activo'); // valido que la persona buscada esté validada y la selecciono
+        cy.get('plex-button[label="Desactivar"]').should('have.attr', 'type', 'warning');
+    });
+})
