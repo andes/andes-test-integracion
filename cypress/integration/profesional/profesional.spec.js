c89d560ae19b19dc8fcdd34cb8bcf4f2e70d70e6
LucasGonzalez <lucasgonzalezm3@gmail.com>
2018-12-04 11:57:20 -0300

    prueba creación de profesionales

--- cypress/integration/profesional/profesional.spec.js
+++ cypress/integration/profesional/profesional.spec.js
@@ -60,4 +60,98 @@ context('Aliasing', () => {
         cy.get('plex-layout-sidebar').should('contain', 'Bioquímica y Nutrición (R) - Matrícula: 533');
 
     });
+
+    it('crear profesional no matriculado, sin validar', () => {
+        cy.server();
+
+        cy.visit(Cypress.env('BASE_URL') + '/tm/profesional/create', {
+            onBeforeLoad: (win) => {
+                win.sessionStorage.setItem('jwt', token);
+            }
+        });
+        cy.get('plex-text[label="Nombre"] input').first().type('Pedro');
+        cy.get('plex-text[label="Apellido"] input').first().type('Ramirez');
+        cy.get('plex-int[label="Número de Documento"] input').type('11111fd111').should('have.value', '11111111'); // verifico que no se pueda ingresar letras
+        cy.get('plex-select[label="Sexo"] input').type('masculino{enter}');
+        cy.get('plex-datetime[label="Fecha de nacimiento"] input').type('05/11/1991{enter}');
+
+        cy.get('plex-phone[label="Número"] input').type('29945876as12').should('have.value', '2994587612');
+        cy.get('plex-button[label="Guardar"]').click();
+        cy.contains('¡El profesional se creó con éxito!');
+    });
+
+    it('crear profesional no matriculado existente en renaper', () => {
+        cy.server();
+        cy.visit(Cypress.env('BASE_URL') + '/tm/profesional/create', {
+            onBeforeLoad: (win) => {
+                win.sessionStorage.setItem('jwt', token);
+            }
+        });
+
+        cy.get('plex-int[label="Número de Documento"] input').type('3588fs799af8').should('have.value', '35887998'); // verifico que no se pueda ingresar letras
+        cy.get('plex-select[label="Sexo"] input').type('masculino{enter}');
+
+        cy.get('plex-layout-sidebar plex-button[label="Validar con servicios de Renaper"]').click();
+        cy.wait(10000);
+
+        // cy.get('plex-text[label="Nombre"] input').should('have.attr', 'readonly', true); // cypress no da soporte todavia a readonly
+        cy.get('plex-text[label="Nombre"] input').should('have.value', 'Daniel Ivan');
+        cy.get('plex-text[label="Apellido"] input').should('have.value', 'SZUBAN MANFREDI');
+        cy.get('plex-datetime[label="Fecha de nacimiento"] input').should('have.value', '05/03/1991');
+
+        cy.get('plex-phone[label="Número"] input').type('29945576as12').should('have.value', '2994557612');
+        cy.get('plex-button[label="Guardar"]').click();
+        cy.contains('¡El profesional se creó con éxito!');
+    });
+
+    it('crear profesional no matriculado no existente en renaper', () => {
+        cy.server();
+        cy.visit(Cypress.env('BASE_URL') + '/tm/profesional/create', {
+            onBeforeLoad: (win) => {
+                win.sessionStorage.setItem('jwt', token);
+            }
+        });
+
+        cy.get('plex-int[label="Número de Documento"] input').type('15e654f898').should('have.value', '15654898'); // verifico que no se pueda ingresar letras
+        cy.get('plex-layout-sidebar').should('not.contain', 'plex-button[label="Validar con servicios de Renaper"]');
+        cy.get('plex-select[label="Sexo"] input').type('femenino{enter}');
+        cy.get('plex-layout-sidebar').find('plex-button[label="Validar con servicios de Renaper"]'); // aparece el boton despues de ingresar documento y sexo
+
+        cy.get('plex-layout-sidebar plex-button[label="Validar con servicios de Renaper"]').click();
+        cy.wait(10000);
+        cy.contains('El profesional no se encontró en RENAPER');
+        cy.get('button[class="swal2-confirm btn btn-warning"]').click();
+
+        cy.get('plex-text[label="Nombre"] input').first().type('Julieta');
+        cy.get('plex-text[label="Apellido"] input').first().type('Rodriguez');
+        cy.get('plex-datetime[label="Fecha de nacimiento"] input').type('05/12/1987{enter}');
+        cy.get('plex-bool[label="No posee ningún tipo de contacto"] input[type="checkbox"]').check({
+            force: true
+        }).should('be.checked');
+        cy.get('plex-button[label="Guardar"]').click();
+        cy.contains('¡El profesional se creó con éxito!');
+    });
+
+    it('crear profesional duplicado', () => {
+        cy.server();
+
+        cy.visit(Cypress.env('BASE_URL') + '/tm/profesional/create', {
+            onBeforeLoad: (win) => {
+                win.sessionStorage.setItem('jwt', token);
+            }
+        });
+        cy.get('plex-text[label="Nombre"] input').first().type('Pedro');
+        cy.get('plex-text[label="Apellido"] input').first().type('Ramirez');
+        cy.get('plex-int[label="Número de Documento"] input').type('11111fd111').should('have.value', '11111111'); // verifico que no se pueda ingresar letras
+        cy.get('plex-select[label="Sexo"] input').type('masculino{enter}');
+        cy.get('plex-datetime[label="Fecha de nacimiento"] input').type('05/11/1991{enter}');
+
+        cy.get('plex-phone[label="Número"] input').type('29945876as12').should('have.value', '2994587612');
+
+        cy.get('plex-button[label="Guardar"]').click();
+        cy.contains('El profesional que está intentando guardar ya se encuentra cargado');
+        cy.get('button[class="swal2-confirm btn btn-warning"]').click();
+        cy.contains('El profesional que está intentando guardar ya se encuentra cargado');
+    });
+
 })
