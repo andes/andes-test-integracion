0c5bc92476d28a3d66dae1f1c61dbffc37e52230
Lautaro Molina <negro89_nqn@hotmail.com>
2019-05-08 11:32:57 -0300

    test para issue #1111

---
+++ cypress/integration/apps/rup/huds.spec.js
@@ -0,0 +1,25 @@
+/// <reference types="Cypress" />
+
+// Test realizado para prueba de issue #1111 basado en la huds de un usuario en la DB de testing-sss.
+
+context('HUDS', () => {
+    let token
+    before(() => {
+        cy.login('34377650', '159753000').then(t => {
+            token = t;
+        })
+    })
+
+
+    it('Color-icono de registros de prestaciones', () => {
+
+        cy.visit(Cypress.env('BASE_URL') + '/rup/vista/586e6e9a27d3107fde18e080', {
+            onBeforeLoad: (win) => {
+                win.sessionStorage.setItem('jwt', token);
+            }
+        })
+        cy.wait(5000);
+        cy.get('button[class="btn btn-block p-0 btn-procedimiento"]').click();
+    });
+
+});
