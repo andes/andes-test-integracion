341b39b597062aba7add763c01207054bdf780c7
Lautaro Molina <negro89_nqn@hotmail.com>
2019-05-08 09:17:15 -0300

    se agrega comentario con instrucciones

--- cypress/integration/apps/reportes/reportes.spec.js
+++ cypress/integration/apps/reportes/reportes.spec.js
@@ -1,9 +1,14 @@
 /// <reference types="Cypress" />
 
+/* Para ejecutar este test correctamente se deben modificar los permisos del
+    usuario que se este usando para loggearse, asignando permisos para reportes
+    en una ejecución y quitandolos para la siguiente.
+*/
+
 context('Reportes', () => {
     let token
     before(() => {
-        cy.login('34377650', '159753000').then(t => {
+        cy.login('34377650', '159753000').then(t => {   // <---- Credenciales que se deben modificar (uss, pass)
             token = t;
         })
     })
