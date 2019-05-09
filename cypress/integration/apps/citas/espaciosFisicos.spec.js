330479e81b94d7f182a2a367dbbdd08680aa937d
Lautaro Molina <negro89_nqn@hotmail.com>
2019-05-09 15:23:29 -0300

    reemplaza 'visit' por 'goto' y utiliza server/route para esperar respuesta de api

--- cypress/integration/apps/citas/espaciosFisicos.spec.js
+++ cypress/integration/apps/citas/espaciosFisicos.spec.js
@@ -12,14 +12,28 @@ context('CITAS - Espacios físicos', () => {
         })
     })
 
-    it('Filtrar espacios físicos', () => {
+    beforeEach(() => {
         cy.goto('/tm/mapa_espacio_fisico', token);
+    })
+
+    it('Filtrar espacios físicos', () => {
+        cy.server()
+        //Rutas para control
+        cy.route('GET', '**api/modules/turnos/agenda?fechaDesde=2019-05-09T03:00:00.000Z&fechaHasta=2019-05-10T02:59:59.999Z&organizacion=57e9670e52df311059bc8964').as('buscarEspacios');
+
         cy.get('plex-select[name="edificio"]').children().children('.selectize-control').click()
             .find('.option[data-value="589c578355bf4277035bcf30"]').click();
         cy.get('plex-button[label="Buscar"]').click();
-        cy.wait(3000);
+        cy.wait('@buscarEspacios').then((xhr) => {
+            expect(xhr.status).to.be.eq(200);
+        });
+        cy.wait(3000);  // tiempo para visualizar los espacios cargados
+
         cy.get('plex-select[name="edificio"]').children().children('.selectize-control').click()
             .find('.option[data-value="589c578355bf4277035bcf2a"]').click();
         cy.get('plex-button[label="Buscar"]').click();
+        cy.wait('@buscarEspacios').then((xhr) => {
+            expect(xhr.status).to.be.eq(200);
+        })
     })
 })
