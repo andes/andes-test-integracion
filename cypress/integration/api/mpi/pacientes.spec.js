b7f12baaa60ed87754c91dc531a5acbdc235afc8
Mariano Andres Botta <marianoabotta@gmail.com>
2019-06-17 19:04:16 -0300

    test(mpi): find paciente by diferent fields

--- cypress/integration/api/mpi/pacientes.spec.js
+++ cypress/integration/api/mpi/pacientes.spec.js
@@ -8,23 +8,23 @@ context('MPI - Pacientes', () => {
     let paciente_c;
 
     before(() => {
-          cy.login('38906735', 'asd').then(t => {
+        cy.login('38906735', 'asd').then(t => {
             token = t;
             cy.fixture('pacientes/paciente-validado').then(paciente => {
                 cy.post('/api/core_v2/mpi/pacientes', paciente, token).then((xhr) => {
-                    id = xhr.body.id;  
+                    id = xhr.body.id;
                 });
             });
 
             cy.fixture('pacientes/paciente-match-a').then(pac_a => {
                 cy.post('/api/core_v2/mpi/pacientes', pac_a, token).then((xhr) => {
-                    paciente_a =  xhr.body;
+                    paciente_a = xhr.body;
                 });
             });
 
             cy.fixture('pacientes/paciente-match-c').then(pac_c => {
                 cy.post('/api/core_v2/mpi/pacientes', pac_c, token).then((xhr) => {
-                    paciente_c =  xhr.body;
+                    paciente_c = xhr.body;
                 });
             });
 
@@ -39,11 +39,11 @@ context('MPI - Pacientes', () => {
     });
 
     it('get paciente ', () => {
-        const request = cy.get('/api/core_v2/mpi/pacientes/59de0ecb96027d180fc4d8d4', token); 
+        const request = cy.get('/api/core_v2/mpi/pacientes/59de0ecb96027d180fc4d8d4', token);
         request.its('status').should('equal', 400);
     });
 
-    it('get paciente - fields query', () => {  
+    it('get paciente - fields query', () => {
         cy.get('/api/core_v2/mpi/pacientes/' + id, token, { fields: 'nombre apellido' }).then(xhr => {
             expect(xhr.status).to.eq(200);
             expect(xhr.body).to.have.property('id', id);
@@ -51,18 +51,34 @@ context('MPI - Pacientes', () => {
             expect(xhr.body).to.not.have.property('documento');
             cy.log(xhr.body)
         });
-        cy.get('/api/core_v2/mpi/pacientes/' + id, token).then(xhr => {
+        cy.get('/api/core_v2/mpi/pacientes/' + id, token, { fields: '-financiador' }).then(xhr => {
             expect(xhr.status).to.eq(200);
-            expect(xhr.body).to.have.property('id', id) 
+            expect(xhr.body).to.have.property('id', id)
         });
- 
+
+    });
+
+    it('search paciente partial name', () => {
+        cy.get('/api/core_v2/mpi/pacientes', token, { nombre: '^ELIZABETH' }).then(xhr => {
+            expect(xhr.status).to.eq(200);
+            expect(xhr.body).to.have.lengthOf(2);
+            // expect(xhr.body[0]).to.have.property('nombre', 'MARIA');
+        });
+    });
+
+    it('search paciente by email', () => {
+        cy.get('/api/core_v2/mpi/pacientes', token, { email: 'hola@andes.gob.ar' }).then(xhr => {
+            expect(xhr.status).to.eq(200);
+            expect(xhr.body).to.have.lengthOf(1);
+            // expect(xhr.body[0]).to.have.property('nombre', 'MARIA');
+        });
     });
 
     it('search paciente by documento', () => {
         cy.get('/api/core_v2/mpi/pacientes', token, { search: '54379999' }).then(xhr => {
             expect(xhr.status).to.eq(200);
             expect(xhr.body).to.have.lengthOf(1);
-            expect(xhr.body[0]).to.have.property('nombre', 'MARIA'); 
+            expect(xhr.body[0]).to.have.property('nombre', 'MARIA');
         });
     });
 
@@ -70,15 +86,15 @@ context('MPI - Pacientes', () => {
         cy.get('/api/core_v2/mpi/pacientes', token, { search: 'maria chavez' }).then(xhr => {
             expect(xhr.status).to.eq(200);
             expect(xhr.body).to.have.lengthOf(1);
-            expect(xhr.body[0]).to.have.property('nombre', 'MARIA'); 
+            expect(xhr.body[0]).to.have.property('nombre', 'MARIA');
         });
     });
 
-    
+
     it('match paciente -debe matchear a 85%', () => {
         let paciente_match;
         cy.fixture('pacientes/paciente-match').then(pac => {
-            paciente_match =  pac;              
+            paciente_match = pac;
             cy.post('/api/core_v2/mpi/pacientes/match', paciente_match, token).then((xhr) => {
                 expect(xhr.status).to.eq(200);
                 expect(xhr.body).to.have.lengthOf(1);
@@ -86,18 +102,18 @@ context('MPI - Pacientes', () => {
             });
         });
     });
-    
+
     it('match paciente - debe obtener una lista de pacientes ordenados por score', () => {
         cy.fixture('pacientes/paciente-match-b').then(pac_b => {
-            paciente_b =  pac_b;  
+            paciente_b = pac_b;
             cy.post('/api/core_v2/mpi/pacientes/match', paciente_b, token).then((xhr) => {
                 expect(xhr.status).to.eq(200);
                 expect(xhr.body).to.have.lengthOf(2);
-                
+
             });
         });
     });
-    
+
     it('post paciente - debe fallar con paciente repetido', () => {
         cy.fixture('pacientes/paciente-validado').then(paciente => {
             cy.post('/api/core_v2/mpi/pacientes', paciente, token).then((xhr) => {
