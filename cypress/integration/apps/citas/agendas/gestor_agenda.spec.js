92dfa9488a7eb681565231558999e479614e00be
nhuenchuman <natalia.huenchuman@gmail.com>
2019-06-26 16:10:20 -0300

    refactor(citas):  actualiza los test del gestor

---
+++ cypress/integration/apps/citas/agendas/gestor_agenda.spec.js
@@ -0,0 +1,293 @@
+describe('CITAS - Gestor Agendas', () => {
+    let token
+    before(() => {
+        cy.login('30643636', 'asd').then(t => {
+            token = t;
+        });
+        cy.viewport(1280, 720);
+    })
+
+    beforeEach(() => {
+        cy.visit(Cypress.env('BASE_URL') + '/citas/gestor_agendas', {
+            onBeforeLoad: (win) => {
+                win.sessionStorage.setItem('jwt', token);
+            }
+
+        });
+    })
+
+    it.only('crea agenda de turnos programados', () => {
+        cy.server();
+        cy.route('PATCH', '**/api/modules/turnos/agenda/**').as('publicar');
+        cy.route('POST', '**/api/modules/turnos/agenda**').as('crear');
+        cy.route('GET', '**/api/modules/turnos/agenda**').as('get');
+
+        cy.get('plex-button[label="Crear una nueva agenda"]').click();
+        cy.swal('cancel');
+        const hoy = Cypress.moment().format('DD/MM/YYYY')
+
+        cy.get('plex-dateTime[name="modelo.fecha"] input').type(hoy).should('have.value', hoy);
+
+        cy.get('plex-dateTime[name="modelo.horaInicio"] input').type('0800').should('have.value', '0800');
+
+        cy.get('plex-dateTime[name="modelo.horaFin"] input').type('1900').should('have.value', '1900');
+
+        cy.get('plex-select[label="Tipos de prestación"]').children().children('.selectize-control').click()
+            .find('.option[data-value="598ca8375adc68e2a0c121b8"]').click()
+
+        cy.get('plex-int[label="Cantidad de Turnos"] input').type(10)
+        cy.get('plex-int[name="accesoDirectoDelDia"] input').type(10);
+        cy.get('plex-button[label="Guardar"]').click();
+
+        cy.wait('@crear').then((xhr) => {
+            expect(xhr.status).to.be.eq(200);
+            expect(xhr.response.body._id).exist();
+        });
+
+
+        // Se filtran las agendas por prestación
+        cy.get('plex-select[label="Prestación"]').children().children('.selectize-control').click()
+            .find('.option[data-value="598ca8375adc68e2a0c121b8"]').click()
+        cy.wait('@get');
+        cy.get('table tr').contains('En planificación').first().click();
+        cy.get('plex-button[title="Publicar"]').click();
+        cy.swal('confirm');
+
+        // Espero a la respuesta de publicar y confirmo que sea StatusCode 200
+        cy.wait('@publicar').then((xhr) => {
+            expect(xhr.status).to.be.eq(200);
+            cy.log(xhr.response.body);
+        });
+
+    });
+
+    it('crear agenda', () => {
+        cy.server();
+        cy.route('PATCH', '**/api/modules/turnos/agenda/**').as('publicar');
+        cy.route('POST', '**/api/modules/turnos/agenda**').as('crear');
+        cy.route('GET', '**/api/modules/turnos/agenda**').as('get');
+
+        cy.get('plex-button[label="Crear una nueva agenda"]').click();
+        cy.swal('cancel');
+        const hoy = Cypress.moment().format('DD/MM/YYYY')
+
+        cy.get('plex-dateTime[name="modelo.fecha"] input').type(hoy).should('have.value', hoy);
+
+        cy.get('plex-dateTime[name="modelo.horaInicio"] input').type('0800').should('have.value', '0800');
+
+        cy.get('plex-dateTime[name="modelo.horaFin"] input').type('1900').should('have.value', '1900');
+
+        cy.get('plex-select[label="Tipos de prestación"]').children().children('.selectize-control').click()
+            .find('.option[data-value="5951051aa784f4e1a8e2afe1"]').click()
+
+        cy.get('plex-int[label="Cantidad de Turnos"] input').type(10)
+        cy.get('plex-int[name="accesoDirectoDelDia"] input').type(10);
+
+
+
+        cy.get('plex-button[label="Guardar"]').click();
+
+        cy.wait('@crear').then((xhr) => {
+            expect(xhr.status).to.be.eq(200)
+        });
+
+        cy.wait('@get');
+        cy.get('table tr').contains('En planificación').first().click();
+        cy.get('plex-button[title="Publicar"]').click();
+        cy.swal('confirm');
+
+        // Espero a la respuesta de publicar y confirmo que sea StatusCode 200
+        cy.wait('@publicar').then((xhr) => {
+            expect(xhr.status).to.be.eq(200)
+        });
+
+
+    });
+
+    it('crea agenda dinámica para la fecha actual', () => {
+        cy.goto('/citas/gestor_agendas', token);
+
+        cy.server();
+        cy.route('PATCH', '**/api/modules/turnos/agenda/**').as('publicar');
+        cy.route('POST', '**/api/modules/turnos/agenda**').as('crear');
+        cy.route('GET', '**/api/modules/turnos/agenda**').as('get');
+
+        cy.get('plex-button[label="Crear una nueva agenda"]').click();
+        cy.swal('cancel');
+
+        const hoy = Cypress.moment().format('DD/MM/YYYY')
+        cy.get('plex-dateTime[name="modelo.fecha"] input').type(hoy).should('have.value', hoy);
+
+        cy.get('plex-dateTime[name="modelo.horaInicio"] input').type('0800').should('have.value', '0800');
+
+        cy.get('plex-dateTime[name="modelo.horaFin"] input').type('1900').should('have.value', '1900');
+
+        cy.get('plex-select[label="Tipos de prestación"]').children().children('.selectize-control').click()
+            .find('.option[data-value="5951051aa784f4e1a8e2afe1"]').click();
+
+        cy.get('plex-bool[name="dinamica"] input[type="checkbox"]').check({
+            force: true
+        }).should('be.checked')
+
+        cy.get('plex-text[name="descripcion"] input').eq(0).type('soy una descripcion', {
+            force: true
+        }).should('have.value', 'soy una descripcion');
+
+
+        cy.get('plex-bool[name="cupo"] input[type="checkbox"]').check({
+            force: true
+        }).should('be.checked')
+
+        cy.get('plex-int[name="cupoMaximo"] input').type('8').should('have.value', '8');
+
+        cy.get('plex-button[label="Guardar"]').click();
+        cy.wait('@crear').then((xhr) => {
+            expect(xhr.status).to.be.eq(200)
+        });
+        cy.wait('@get');
+
+        cy.get('table tr').contains('En planificación').first().click();
+        cy.get('plex-button[title="Publicar"]').click();
+        cy.swal('confirm');
+
+        // Espero a la respuesta de publicar y confirmo que sea StatusCode 200
+        cy.wait('@publicar').then((xhr) => {
+            expect(xhr.status).to.be.eq(200)
+        });
+    });
+
+    it('crea agenda semana próxima y publicarla', () => {
+        cy.visit(Cypress.env('BASE_URL') + '/citas/gestor_agendas', {
+            onBeforeLoad: (win) => {
+                win.sessionStorage.setItem('jwt', token);
+            }
+        });
+        cy.server();
+
+        cy.get('plex-button[label="Crear una nueva agenda"]').click();
+        cy.get('div').then(($body) => {
+            if ($body.hasClass('swal2-container')) {
+                cy.get('.swal2-cancel').click({
+                    force: true
+                })
+            }
+        })
+        let proximaSemana = Cypress.moment().add(7, 'days').format('DD/MM/YYYY');
+        cy.get('plex-dateTime[name="modelo.fecha"] input').type(proximaSemana);
+        cy.get('plex-dateTime[name="modelo.horaInicio"] input').type('10');
+        cy.get('plex-dateTime[name="modelo.horaFin"] input').type('15');
+        cy.get('plex-select[name="modelo.tipoPrestaciones"] input').type('consulta de medicina general (procedimiento){enter}');
+        cy.get('plex-select[name="modelo.profesionales"] input').type('alvarez angelica vanesa', {
+            force: true
+        });
+        cy.wait(1000); // TODO no darle enter hasta que se haya cargado el profesional
+        cy.get('plex-select[name="modelo.profesionales"] input').type('{enter}');
+        cy.get('plex-int[name="cantidadTurnos"] input').type('10');
+        cy.get('plex-int[name="accesoDirectoDelDia"] input').type('2');
+        cy.get('plex-int[name="accesoDirectoProgramado"] input').type('3');
+        cy.get('plex-int[name="reservadoGestion"] input').type('3');
+        cy.get('plex-int[name="reservadoProfesional"] input').type('2');
+        cy.get('plex-button[label="Guardar"]').click({
+            force: true
+        });
+
+        // publico la agenda
+
+        cy.get('plex-datetime[name="fechaDesde"] input').type('{selectall}{backspace}' + proximaSemana);
+        cy.get('plex-datetime[name="fechaHasta"] input').type('{selectall}{backspace}' + proximaSemana);
+        cy.wait(15000);
+        cy.route('GET', '**/api/modules/turnos/agenda/5c2ca5df9b2ff016c7198f0f').as('botonAgenda');
+        cy.get('table tbody div').contains('ALVAREZ, ANGELICA VANESA').click({
+            force: true
+        });
+
+        cy.wait('@botonAgenda').then(() => {
+            cy.get('botones-agenda plex-button[title="Publicar"]').click();
+            cy.get('button').contains('CONFIRMAR').click();
+        });
+    })
+
+    it('clona agenda repetida y debería fallar', () => {
+        cy.server();
+        cy.route('PATCH', '**/api/modules/turnos/agenda/**').as('publicar');
+        cy.route('POST', '**/api/modules/turnos/agenda**').as('crear');
+        cy.route('GET', '**/api/modules/turnos/agenda**').as('getAgenda');
+        cy.route('GET', '**/api/core/tm/profesionales**').as('getProfesional');
+
+        // Se crea la primer agenda..
+        cy.get('plex-button[label="Crear una nueva agenda"]').click();
+        cy.swal('cancel');
+        const hoy = Cypress.moment().format('DD/MM/YYYY')
+
+        cy.get('plex-dateTime[name="modelo.fecha"] input').type(hoy).should('have.value', hoy);
+
+        cy.get('plex-dateTime[name="modelo.horaInicio"] input').type('0800').should('have.value', '0800');
+
+        cy.get('plex-dateTime[name="modelo.horaFin"] input').type('0900').should('have.value', '0900');
+
+        cy.get('plex-select[name="modelo.tipoPrestaciones"]').children().children('.selectize-control').click()
+            .find('.option[data-value="5951051aa784f4e1a8e2afe1"]').click({
+                force: true
+            });
+
+        cy.get('plex-select[label="Equipo de Salud"]').children().children('.selectize-control').find('input').first().type('MONTEVERDE MARIA LAURA').as('profesional');
+        cy.wait('@getProfesional');
+        cy.get('@profesional').type('{enter}');
+
+        cy.get('plex-int[label="Cantidad de Turnos"] input').type(10)
+        cy.get('plex-int[name="accesoDirectoDelDia"] input').type(10);
+
+        cy.get('plex-button[label="Guardar"]').click();
+
+        cy.wait('@crear').then((xhr) => {
+            expect(xhr.status).to.be.eq(200)
+        });
+
+        cy.wait(1000); // Solo para observar el toast, luego se lo clickea para ocultarlo
+        cy.get('simple-notification').children('.simple-notification').first().click();
+
+        // Se crea la segunda agenda ..
+        cy.get('plex-button[label="Crear una nueva agenda"]').click();
+        cy.swal('cancel');
+        const manana = Cypress.moment(new Date()).add(1, 'days').format('DD/MM/YYYY');
+
+        cy.get('plex-dateTime[name="modelo.fecha"] input').type(manana).should('have.value', manana);
+
+        cy.get('plex-dateTime[name="modelo.horaInicio"] input').type('0800').should('have.value', '0800');
+
+        cy.get('plex-dateTime[name="modelo.horaFin"] input').type('0900').should('have.value', '0900');
+
+        cy.get('plex-select[name="modelo.tipoPrestaciones"]').children().children('.selectize-control').click()
+            .find('.option[data-value="5951051aa784f4e1a8e2afe1"]').click({
+                force: true
+            });
+
+        cy.get('plex-select[label="Equipo de Salud"]').children().children('.selectize-control').find('input').first().type('MONTEVERDE MARIA LAURA').as('profesional');
+        cy.wait('@getProfesional');
+        cy.get('@profesional').type('{enter}');
+
+        cy.get('plex-int[label="Cantidad de Turnos"] input').type(10)
+        cy.get('plex-int[name="accesoDirectoDelDia"] input').type(10);
+
+        cy.get('plex-button[label="Guardar"]').click();
+        debugger;
+        cy.wait('@crear').then((xhr) => {
+            expect(xhr.status).to.be.eq(200);
+            debugger;
+            cy.log(xhr);
+        });
+
+        // Se obtiene la primera agenda con estado 'En planificación'        
+        cy.get('tbody tr').find('span').contains('En planificación').first().click({
+            force: true
+        });
+
+        // Se intenta clonar la agenda obtenida
+        cy.get('botones-agenda').find('plex-button[title="Clonar"]').first().click();
+
+        cy.get('tbody tr').children('.outline-success').first().next().click();
+
+        // cy.get('plex-button[label="Clonar Agenda"]').click();
+    })
+
+})
---
+++ cypress/integration/apps/citas/turnos/punto_inicio.spec.js
@@ -0,0 +1,65 @@
+context('Agenda dinamicas', () => {
+    let token
+    before(() => {
+        cy.login('38906735', 'asd').then(t => {
+            token = t;
+            cy.createPaciente('paciente-dinamico', token);
+        });
+    })
+
+    beforeEach(() => {
+
+    })
+
+
+
+    it('dar turno agenda dinamica', () => {
+        cy.server();
+        cy.route('PATCH', '**/api/modules/turnos/turno/**').as('darTurno');
+        cy.route('GET', '**api/core/tm/tiposPrestaciones**').as('prestaciones');
+
+        cy.goto('/citas/puntoInicio', token);
+
+        cy.get('plex-text input[type=text]').first().type('38906735').should('have.value', '38906735');
+        cy.get('tr').first().click();
+        cy.get('plex-button[title="Dar Turno"]').click();
+        cy.wait('@prestaciones');
+
+        cy.get('plex-select[name="tipoPrestacion"]').children().children('.selectize-control').click()
+            .find('.option[data-value="5951051aa784f4e1a8e2afe1"]').click();
+
+        cy.get('.outline-success ').first().click();
+        cy.get('plex-button[label="Dar Turno"]').click();
+        cy.get('plex-button[label="Confirmar"]').click();
+
+        // Confirmo que se dio el turno desde la API
+        cy.wait('@darTurno').then((xhr) => {
+            expect(xhr.status).to.be.eq(200)
+        });
+    });
+
+    it('dar turno programado', () => {
+        cy.server();
+        cy.route('PATCH', '**/api/modules/turnos/turno/**').as('darTurno');
+        cy.route('GET', '**api/core/tm/tiposPrestaciones**').as('prestaciones');
+
+        cy.goto('/citas/puntoInicio', token);
+
+        cy.get('plex-text input[type=text]').first().type('38906734').should('have.value', '38906734');
+        cy.get('tr').first().click();
+        cy.get('plex-button[title="Dar Turno"]').click();
+        cy.wait('@prestaciones');
+
+        cy.get('plex-select[name="tipoPrestacion"]').children().children('.selectize-control').click()
+            .find('.option[data-value="5951051aa784f4e1a8e2afe1"]').click();
+
+        cy.get('.outline-success ').first().click();
+        cy.get('div').contains('08:00').first().click()
+        cy.get('plex-button[label="Confirmar"]').click();
+
+        // Confirmo que se dio el turno desde la API
+        cy.wait('@darTurno').then((xhr) => {
+            expect(xhr.status).to.be.eq(200)
+        });
+    });
+});
