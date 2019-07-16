6cf863a1dcface786eff616d2e5735721e9c3500
LucasGonzalez <lucasgonzalezm3@gmail.com>
2019-07-16 14:14:51 -0300

    fix(citas): da turno en agenda dinámica

--- cypress/integration/apps/citas/turnos/punto_inicio.spec.js
+++ cypress/integration/apps/citas/turnos/punto_inicio.spec.js
@@ -15,13 +15,18 @@ context('Agenda dinamicas', () => {
 
     it.only('dar turno agenda dinámica', () => {
         cy.server();
+        cy.route('GET', '**api/core/mpi/pacientes?**').as('busquedaPaciente');
         cy.route('PATCH', '**/api/modules/turnos/turno/**').as('darTurno');
         cy.route('GET', '**api/core/tm/tiposPrestaciones**').as('prestaciones');
 
         cy.goto('/citas/punto-inicio', token);
 
         cy.get('plex-text input[type=text]').first().type('38906735').should('have.value', '38906735');
-        cy.get('tr').first().click();
+        cy.wait('@busquedaPaciente').then((xhr) => {
+            expect(xhr.status).to.be.eq(200);
+        });
+        cy.get('paciente-listado').find('td').contains('38906735').click();
+        
         cy.get('plex-button[title="Dar Turno"]').click();
         cy.wait('@prestaciones');
 
@@ -29,7 +34,7 @@ context('Agenda dinamicas', () => {
             .find('.option[data-value="5951051aa784f4e1a8e2afe1"]').click();
 
         cy.get('.outline-success ').first().click();
-        cy.get('plex-button[label="Dar Turno"]').click();
+        cy.get('dar-turnos div').contains('08:00').click();
         cy.get('plex-button[label="Confirmar"]').click();
 
         // Confirmo que se dio el turno desde la API
