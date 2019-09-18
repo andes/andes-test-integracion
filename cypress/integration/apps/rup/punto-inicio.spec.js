e17f3d9885f0365c99366893a61a2a42a126d3a3
LucasGonzalez <lucasgonzalezm3@gmail.com>
2019-09-18 08:30:08 -0300

    feat(rup): agrega prueba iniciar y validar prestacion niño sano a los 5 años

--- cypress/integration/apps/rup/punto-inicio.spec.js
+++ cypress/integration/apps/rup/punto-inicio.spec.js
@@ -235,4 +235,88 @@ context('RUP - Punto de inicio', () => {
             expect(xhr.response.body.estados[2]).to.be.undefined;
         });
     })
+    it('Iniciar y validar prestación niño sano 5 años - Fuera de agenda', () => {
+        cy.goto('/rup', token);
+
+        cy.server();
+        const fixtures = [];
+        const requeridos = [];
+        cy.fixture('conceptos-snomed-ninoSano.json').then(json => {
+            fixtures.push(...json);
+        });
+        cy.fixture('conceptos-snomed-requeridosNinoSano1Mes.json').then(json => {
+            requeridos.push(...json);
+        });
+
+        // Stub
+        cy.route(/api\/core\/term\/snomed\?/, fixtures).as('search');
+        cy.route('GET', '**api/core/tm/tiposPrestaciones**').as('prestaciones');
+        cy.route('POST', '**/api/modules/rup/prestaciones').as('create');
+        cy.route('GET', '/api/modules/obraSocial/os/**', []).as('obraSocial');
+        cy.route('GET', '/api/modules/obraSocial/puco/**', []).as('version');
+        cy.route('PATCH', 'api/modules/rup/prestaciones/**').as('patch');
+        cy.route('GET', 'api/core/term/snomed/expression?expression=^2501000013104', requeridos).as('requeridos')
+
+        cy.get('plex-button[label="PACIENTE FUERA DE AGENDA"]').click({
+            force: true
+        });
+
+        cy.wait('@prestaciones');
+        cy.get('plex-select[name="nombrePrestacion"]').children().children('.selectize-control').click()
+            .find('.option[data-value="57f5069d69fe79a5980b072f"]').click();
+
+        cy.get('plex-button[label="SELECCIONAR PACIENTE"]').click({
+            force: true
+        });
+        cy.get('plex-text input').first().type('55687645');
+        cy.get('table tbody tr').first().click();
+
+        cy.get('plex-button[type="success"]').contains('INICIAR PRESTACIÓN').click();
+        cy.wait('@create').then((xhr) => {
+            expect(xhr.status).to.be.eq(200);
+            expect(xhr.response.body.solicitud.turno).to.be.undefined;
+            expect(xhr.response.body.solicitud.tipoPrestacion.id).to.be.eq('57f5069d69fe79a5980b072f');
+            expect(xhr.response.body.paciente.documento).to.be.eq('55687645');
+            expect(xhr.response.body.estados[0].tipo).to.be.eq('ejecucion');
+        });
+        cy.wait(3000); // le da tiempo para que cargue el sidebar
+        cy.get('plex-text input').first().type('5 años');
+        cy.wait(3000);
+
+        cy.get('.mdi-plus').first().click();
+
+        cy.get('rup-peso input').eq(0).type('57');
+        cy.get('rup-talla input').eq(0).type('100');
+        cy.get('rup-percentilo-talla input').eq(0).type('65');
+        cy.get('rup-percentilo-imc input').eq(0).type('76');
+        cy.get('rup-tension-sistolica input').eq(0).type('75');
+        cy.get('rup-tension-diastolica input').eq(0).type('70');
+        cy.get('rup-percentilo-de-tension-arterial input').eq(0).type('68');
+        cy.get('rup-desarrollo-psicomotor plex-bool').eq(1).click();
+        cy.get('span').contains('Guardar consulta de niño sano').click({
+            force: true
+        });
+        cy.wait('@patch').then((xhr) => {
+            expect(xhr.status).to.be.eq(200);
+            expect(xhr.response.body.solicitud.turno).to.be.undefined;
+            expect(xhr.response.body.solicitud.tipoPrestacion.id).to.be.eq('57f5069d69fe79a5980b072f');
+            expect(xhr.response.body.paciente.documento).to.be.eq('55687645');
+            expect(xhr.response.body.estados[0].tipo).to.be.eq('ejecucion');
+            expect(xhr.response.body.estados[1]).to.be.undefined;
+        });
+        cy.wait(1000); // da tiempo para que el boton cambie de cartel
+        cy.get('span').contains('Validar consulta de niño sano').first().click({
+            force: true
+        });
+        cy.get('button').contains('CONFIRMAR').click();
+
+        cy.wait('@patch').then((xhr) => {
+            expect(xhr.status).to.be.eq(200);
+            expect(xhr.response.body.solicitud.turno).to.be.undefined;
+            expect(xhr.response.body.solicitud.tipoPrestacion.id).to.be.eq('57f5069d69fe79a5980b072f');
+            expect(xhr.response.body.paciente.documento).to.be.eq('55687645');
+            expect(xhr.response.body.estados[1].tipo).to.be.eq('validada');
+            expect(xhr.response.body.estados[2]).to.be.undefined;
+        });
+    })
 });
