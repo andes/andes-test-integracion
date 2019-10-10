fb8024f5a09ccacec1d63e5f18ebcb4f0cf9999a
LucasGonzalez <lucasgonzalezm3@gmail.com>
2019-10-10 15:43:04 -0300

    refactor(citas): mueve de lugar pruebas de dar turnos a nuevo spec dedicado a la dacion de turnos
    Se encontraban en el punto de inicio de citas

--- cypress/integration/apps/citas/turnos/dar_turno.spec.js
+++ cypress/integration/apps/citas/turnos/dar_turno.spec.js
@@ -5,6 +5,8 @@ context('punto de inicio', () => {
         cy.seed();
         cy.login('30643636', 'asd').then(t => {
             token = t;
+            cy.createAgenda('apps/citas/turnos/agendaDinamicaDarTurno', 0, 0, 1, token);
+            cy.createAgenda('apps/citas/turnos/agendaDarTurnoProgramado', 8, null, null, token);
             cy.createPaciente('paciente-normal.json', token);
         });
     });
@@ -13,26 +15,56 @@ context('punto de inicio', () => {
         cy.goto('/citas/punto-inicio', token);
         cy.route('GET', '**api/core/mpi/pacientes?**').as('busquedaPaciente');
         cy.route('GET', '**api/core/log/paciente?idPaciente=**').as('seleccionPaciente');
+        cy.darTurno('**api/core/mpi/pacientes/57f3b5d579fe79a598e6281f', token);
 
-        cy.plexText('name="buscador"', '38906734');
-        cy.wait('@busquedaPaciente').then((xhr) => {
-            expect(xhr.status).to.be.eq(200);
-        });
-        cy.get('paciente-listado table').find('td').contains('38906734').click();
-        cy.wait('@seleccionPaciente').then((xhr) => {
-            expect(xhr.status).to.be.eq(200);
-        });
-        cy.plexButtonIcon('calendar-plus').click({force: true});
-        cy.wait('@darTurnoPaciente').then((xhr) => {
-            expect(xhr.status).to.be.eq(200);
-        });
     })
-    it('Buscar paciente inexistente', () => {
-        cy.plexText('name="buscador"', '12362920');
-        cy.wait('@busquedaPaciente').then((xhr) => {
-            expect(xhr.status).to.be.eq(200);
+    it('dar turno agenda dinámica', () => {
+        cy.route('GET', '**api/core/mpi/pacientes?**').as('busquedaPaciente');
+        cy.route('PATCH', '**/api/modules/turnos/turno/**').as('darTurno');
+        cy.route('GET', '**/api/modules/turnos/agenda?rango=true&desde=**').as('cargaAgendas');
+        cy.route('GET', '**/api/modules/turnos/agenda/**').as('seleccionAgenda');
+        cy.route('GET', '**api/core/tm/tiposPrestaciones**').as('prestaciones');
+
+        cy.wait('@prestaciones');
+        cy.selectOption('name="tipoPrestacion"', '"598ca8375adc68e2a0c121d5"');
+        cy.wait('@cargaAgendas');
+        cy.get('app-calendario .dia').contains(Cypress.moment().date()).click();
+        cy.wait('@seleccionAgenda').then((xhr) => {
+            expect(xhr.status).to.be.eq(200)
         });
-        cy.get('.alert.alert-danger').should('contain', 'No se encontró ningún paciente..');
-    })
-    
+        // cy.get('plex-button[label="Dar Turno"]').click();
+        cy.plexButton('Dar Turno').click();
+        cy.plexButton('Confirmar').click();
+        // Confirmo que se dio el turno desde la API
+        cy.wait('@darTurno').then((xhr) => {
+            expect(xhr.status).to.be.eq(200)
+        });
+    });
+
+    it('dar turno programado', () => {
+        cy.route('GET', '**api/core/tm/tiposPrestaciones**').as('prestaciones');
+        cy.route('GET', '**/api/modules/turnos/agenda?rango=true&desde=**').as('cargaAgendas');
+        cy.route('GET', '**/api/modules/turnos/agenda/**').as('seleccionAgenda');
+        cy.route('PATCH', '**/api/modules/turnos/turno/**').as('darTurno');
+
+        cy.wait('@prestaciones');
+        cy.selectOption('name="tipoPrestacion"', '"598ca8375adc68e2a0c121d5"');
+        if (Cypress.moment().add(8, 'days').month() > Cypress.moment().month()) {
+            cy.get('plex-button[icon="chevron-right"]').click();
+        }
+        cy.wait('@cargaAgendas');
+        cy.wait(1000);
+        cy.get('div[class="dia"]').contains(Cypress.moment().add(8, 'days').format('D')).click();
+        cy.wait('@seleccionAgenda').then((xhr) => {
+            expect(xhr.status).to.be.eq(200)
+        });
+        cy.get('dar-turnos div[class="text-center hover p-2 mb-3 outline-dashed-default"]').first().click();
+        cy.plexButton('Confirmar').click();
+        // Confirmo que se dio el turno desde la API
+        cy.wait('@darTurno').then((xhr) => {
+            expect(xhr.status).to.be.eq(200)
+        });
+    });
+
+
 });
