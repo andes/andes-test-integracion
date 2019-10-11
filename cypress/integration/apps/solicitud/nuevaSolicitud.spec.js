d1e2b4431f26b5aceec89d83869aba60df1c1765
Mariano Andres Botta <marianoabotta@gmail.com>
2019-10-11 09:45:39 -0300

    feat(solicitud): paciente expecial para solicitudes

--- cypress/integration/apps/solicitud/nuevaSolicitud.spec.js
+++ cypress/integration/apps/solicitud/nuevaSolicitud.spec.js
@@ -12,14 +12,15 @@ function seleccionarPaciente(dni) {
     cy.get('paciente-listado').find('td').contains(dni).click();
 }
 
-context('TOP: Nueva Solicitud de Salida', () => {
-    let token
+describe('TOP: Nueva Solicitud de Salida', () => {
+    let token, dni;
     before(() => {
         cy.seed();
         cy.viewport(1280, 720);
         cy.login('30643636', 'asd').then(t => {
             token = t;
-            cy.createPaciente('paciente-normal', token);
+            cy.createPaciente('apps/solicitud/paciente-nueva-solicitud', token);
+            dni = "2006890";
         });
     })
 
@@ -33,7 +34,7 @@ context('TOP: Nueva Solicitud de Salida', () => {
 
     it('nueva solicitud exitosa', () => {
         let idPrestacion;
-        seleccionarPaciente('NORMAL');
+        seleccionarPaciente(dni);
         cy.plexDatetime('label="Fecha en que el profesional solicitó la prestación"', cy.today());
         cy.get('div a.introjs-button.introjs-skipbutton.introjs-donebutton').click();
         cy.plexSelectAsync('label="Tipos de Prestación Origen"', 'Consulta de esterilidad', '@tipoPrestacion', '59ee2d9bf00c415246fd3d1c');
@@ -47,13 +48,13 @@ context('TOP: Nueva Solicitud de Salida', () => {
         cy.plexButton('Guardar').click();
         cy.wait('@createSolicitud').then((xhr) => {
             expect(xhr.status).to.be.eq(200);
-            expect(xhr.response.body.paciente.documento).to.be.eq('38906734');
+            expect(xhr.response.body.paciente.documento).to.be.eq(dni);
             expect(xhr.response.body.solicitud.tipoPrestacion.conceptId).to.be.eq(idPrestacion);
         });
     });
 
     it('campos requeridos', () => {
-        seleccionarPaciente('38906734');
+        seleccionarPaciente(dni);
 
         cy.get('div a.introjs-button.introjs-skipbutton.introjs-donebutton').click();
         cy.plexButton('Guardar').click();
@@ -95,7 +96,7 @@ context('TOP: Nueva Solicitud de Salida', () => {
     });
 
     it('comprobación de reglas', () => {
-        seleccionarPaciente('38906734');
+        seleccionarPaciente(dni);
         cy.plexDatetime('label="Fecha en que el profesional solicitó la prestación"', cy.today());
         cy.get('div a.introjs-button.introjs-skipbutton.introjs-donebutton').click();
         cy.plexSelectType('label="Organización destino"').find('.selectize-dropdown-content').children().should('have.length', 0);
