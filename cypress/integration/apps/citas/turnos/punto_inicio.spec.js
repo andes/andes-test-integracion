2ba7ba4f2de752c604a858562d33a490dc85395c
Gabo <cancellieri.gabriel@gmail.com>
2019-10-24 15:23:30 -0300

    feat(CITAS): Se agregaron tests en punto de inicio de citas

--- cypress/integration/apps/citas/turnos/punto_inicio.spec.js
+++ cypress/integration/apps/citas/turnos/punto_inicio.spec.js
@@ -6,11 +6,15 @@ context('punto de inicio', () => {
         cy.seed();
         cy.login('30643636', 'asd').then(t => {
             token = t;
-            cy.createPaciente('paciente-normal.json', token);
+            cy.createPaciente('paciente-normal.json', token).then(r => {
+                cy.createAgendaPaciente('agenda-turno-paciente.json', 0, 0, 2, r.body._id, token);
+                cy.createAgendaPaciente('agenda-turno-paciente.json', 0, 4, 6, r.body._id, token);
+            });
         });
     });
 
     beforeEach(() => {
+        cy.viewport(1280, 720);
         cy.server();
         cy.goto('/citas/punto-inicio', token);
         cy.route('GET', '**api/core/mpi/pacientes?**').as('busquedaPaciente');
@@ -66,4 +70,240 @@ context('punto de inicio', () => {
         cy.plexButton('Activar App Mobile').click();
         cy.swal('confirm')
     })
+
+    it('editar datos de contacto', () => {
+        cy.route('GET', '**/api/core/mpi/pacientes/**').as('getPaciente');
+        cy.route('GET', '**/api/modules/turnos/historial?**').as('getTurnos');
+        cy.route('GET', '**/api/core/log/paciente?**').as('getLog');
+        cy.route('GET', '**/api/modules/obraSocial/puco/**').as('getObraSocial');
+        cy.route('GET', '**/api/core/tm/paises?**').as('getPaises');
+        cy.route('GET', '**/api/modules/obraSocial/prepagas**').as('getPrepagas');
+        cy.route('GET', '**/api/core/tm/provincias**').as('getProvincias');
+        cy.route('GET', '**/api/core/tm/provincias?**').as('getNeuquen');
+        cy.route('GET', '**/api/core/tm/localidades**').as('provincia');
+        cy.route('GET', '**/api/core/tm/barrios**').as('localidad');
+        cy.route('PUT', '**/api/core/mpi/pacientes/**').as('guardar');
+
+        cy.plexText('name="buscador"', DNI);
+        cy.wait('@busquedaPaciente').then((xhr) => {
+            expect(xhr.status).to.be.eq(200);
+        });
+
+        cy.get('paciente-listado').find('td').contains(DNI).click();
+
+        cy.wait('@getPaciente').then((xhr) => {
+            expect(xhr.status).to.be.eq(200);
+        });
+
+        cy.wait('@getTurnos').then((xhr) => {
+            expect(xhr.status).to.be.eq(200);
+        });
+
+        cy.wait('@getLog').then((xhr) => {
+            expect(xhr.status).to.be.eq(200);
+        });
+
+        cy.wait('@getObraSocial').then((xhr) => {
+            expect(xhr.status).to.be.eq(200);
+        });
+
+        cy.wait('@getPaises').then((xhr) => {
+            expect(xhr.status).to.be.eq(200);
+        });
+
+        cy.wait('@getPrepagas').then((xhr) => {
+            expect(xhr.status).to.be.eq(200);
+        });
+
+        cy.wait('@getProvincias').then((xhr) => {
+            expect(xhr.status).to.be.eq(200);
+        });
+
+        cy.wait('@getNeuquen').then((xhr) => {
+            expect(xhr.status).to.be.eq(200);
+        });
+
+        cy.plexPhone('label="Número"', '2990000000');
+        cy.plexText('label="Dirección"', 'Avenida Las Flores 1200');
+
+        cy.plexSelectType('label="Provincia"', 'Neuquén');
+        cy.wait('@provincia').then((xhr) => {
+            expect(xhr.status).to.be.eq(200);
+        });
+
+        cy.plexSelectType('label="Localidad"', 'Neuquén');
+        cy.wait('@localidad').then((xhr) => {
+            expect(xhr.status).to.be.eq(200);
+        });
+
+        cy.plexButton("Guardar").click();
+        cy.wait('@guardar').then((xhr) => {
+            expect(xhr.status).to.be.eq(200);
+        });
+
+        cy.contains('Los cambios han sido guardados');
+    });
+
+    it('dar asistencia y quitarla', () => {
+        cy.route('GET', '**/api/core/mpi/pacientes/**').as('getPaciente');
+        cy.route('GET', '**/api/modules/turnos/historial?**').as('getTurnos');
+        cy.route('GET', '**/api/core/log/paciente?**').as('getLog');
+        cy.route('GET', '**/api/modules/obraSocial/puco/**').as('getObraSocial');
+        cy.route('GET', '**/api/core/tm/paises?**').as('getPaises');
+        cy.route('GET', '**/api/modules/obraSocial/prepagas**').as('getPrepagas');
+        cy.route('GET', '**/api/core/tm/provincias**').as('getProvincias');
+        cy.route('GET', '**/api/core/tm/provincias?**').as('getNeuquen');
+        cy.route('PATCH', '**/api/modules/turnos/agenda/**').as('patchAgenda');
+
+        cy.plexText('name="buscador"', DNI);
+        cy.wait('@busquedaPaciente').then((xhr) => {
+            expect(xhr.status).to.be.eq(200);
+        });
+
+        cy.get('paciente-listado').find('td').contains(DNI).click();
+
+        cy.wait('@getPaciente').then((xhr) => {
+            expect(xhr.status).to.be.eq(200);
+        });
+
+        cy.wait('@getTurnos').then((xhr) => {
+            expect(xhr.status).to.be.eq(200);
+        });
+
+        cy.wait('@getLog').then((xhr) => {
+            expect(xhr.status).to.be.eq(200);
+        });
+
+        cy.wait('@getObraSocial').then((xhr) => {
+            expect(xhr.status).to.be.eq(200);
+        });
+
+        cy.wait('@getPaises').then((xhr) => {
+            expect(xhr.status).to.be.eq(200);
+        });
+
+        cy.wait('@getPrepagas').then((xhr) => {
+            expect(xhr.status).to.be.eq(200);
+        });
+
+        cy.get('plex-tabs').contains('Turnos').click({ force: true });
+
+        cy.wait('@getTurnos').then((xhr) => {
+            expect(xhr.status).to.be.eq(200);
+        });
+
+        cy.wait('@getLog').then((xhr) => {
+            expect(xhr.status).to.be.eq(200);
+        });
+
+        cy.get('li[class="list-group-item"]').find('div[class=" list-group-item-text"]').find('div[class="row"]')
+            .find('div[class="col-md-12"]').eq(1).plexButton("Dar Asistencia").click();
+
+        cy.wait('@patchAgenda').then((xhr) => {
+            expect(xhr.status).to.be.eq(200);
+        });
+
+        cy.contains('Se registro la asistencia del paciente');
+
+        cy.wait('@getTurnos').then((xhr) => {
+            expect(xhr.status).to.be.eq(200);
+        });
+
+        cy.wait('@getLog').then((xhr) => {
+            expect(xhr.status).to.be.eq(200);
+        });
+
+        cy.get('li[class="list-group-item"]').find('div[class=" list-group-item-text"]').find('div[class="row"]')
+            .find('div[class="col-md-12"]').eq(1).plexButton("Quitar Asistencia").click({ force: true });
+
+        cy.wait('@patchAgenda').then((xhr) => {
+            expect(xhr.status).to.be.eq(200);
+        });
+
+        cy.contains('Se registro la inasistencia del paciente');
+
+        cy.wait('@getTurnos').then((xhr) => {
+            expect(xhr.status).to.be.eq(200);
+        });
+
+        cy.wait('@getLog').then((xhr) => {
+            expect(xhr.status).to.be.eq(200);
+        });
+    });
+
+    it('liberar turno', () => {
+        cy.route('GET', '**/api/core/mpi/pacientes/**').as('getPaciente');
+        cy.route('GET', '**/api/modules/turnos/historial?**').as('getTurnos');
+        cy.route('GET', '**/api/core/log/paciente?**').as('getLog');
+        cy.route('GET', '**/api/modules/obraSocial/puco/**').as('getObraSocial');
+        cy.route('GET', '**/api/core/tm/paises?**').as('getPaises');
+        cy.route('GET', '**/api/modules/obraSocial/prepagas**').as('getPrepagas');
+        cy.route('GET', '**/api/core/tm/provincias**').as('getProvincias');
+        cy.route('GET', '**/api/core/tm/provincias?**').as('getNeuquen');
+        cy.route('GET', '**/api/modules/turnos/agenda/**').as('getTurnosAgenda');
+        cy.route('PATCH', '**/api/modules/turnos/agenda/**').as('patchAgenda');
+
+        cy.plexText('name="buscador"', DNI);
+        cy.wait('@busquedaPaciente').then((xhr) => {
+            expect(xhr.status).to.be.eq(200);
+        });
+
+        cy.get('paciente-listado').find('td').contains(DNI).click();
+
+        cy.wait('@getPaciente').then((xhr) => {
+            expect(xhr.status).to.be.eq(200);
+        });
+
+        cy.wait('@getTurnos').then((xhr) => {
+            expect(xhr.status).to.be.eq(200);
+        });
+
+        cy.wait('@getLog').then((xhr) => {
+            expect(xhr.status).to.be.eq(200);
+        });
+
+        cy.wait('@getObraSocial').then((xhr) => {
+            expect(xhr.status).to.be.eq(200);
+        });
+
+        cy.wait('@getPaises').then((xhr) => {
+            expect(xhr.status).to.be.eq(200);
+        });
+
+        cy.wait('@getPrepagas').then((xhr) => {
+            expect(xhr.status).to.be.eq(200);
+        });
+
+        cy.get('plex-tabs').contains('Turnos').click({ force: true });
+
+        cy.wait('@getTurnos').then((xhr) => {
+            expect(xhr.status).to.be.eq(200);
+        });
+
+        cy.wait('@getLog').then((xhr) => {
+            expect(xhr.status).to.be.eq(200);
+        });
+
+        cy.get('li[class="list-group-item"]').find('div[class=" list-group-item-text"]').find('div[class="row"]')
+            .find('div[class="col-md-12"]').eq(1).plexButton("Liberar Turno").click({ force: true });
+
+            cy.wait('@getTurnosAgenda').then((xhr) => {
+                expect(xhr.status).to.be.eq(200);
+            });
+
+        cy.plexButton("Liberar").click();
+
+        cy.wait('@patchAgenda').then((xhr) => {
+            expect(xhr.status).to.be.eq(200);
+        });
+
+        cy.wait('@getTurnos').then((xhr) => {
+            expect(xhr.status).to.be.eq(200);
+        });
+
+        cy.wait('@getLog').then((xhr) => {
+            expect(xhr.status).to.be.eq(200);
+        });
+
+    });
 })
--- cypress/support/seed.js
+++ cypress/support/seed.js
@@ -103,6 +103,68 @@ Cypress.Commands.add('createAgenda48hs', (name, token) => {
     });
 });
 
+Cypress.Commands.add('createAgendaPaciente', (name, daysOffset, horaInicioOffset, horaFinOffset, idPaciente, token) => {
+    return cy.fixture(name).then((agenda) => {
+        agenda.bloques[0].turnos[0].paciente.id = idPaciente;
+        if (horaInicioOffset !== null) {
+            let newDate = Cypress.moment().add(daysOffset, 'days'); //.format('YYYY-MM-DD');
+            let newFechaInicio = Cypress.moment().set({
+                'year': newDate.year(),
+                'month': newDate.month(),
+                'date': newDate.date(),
+                'hour': Cypress.moment().add(horaInicioOffset, 'hours').format('HH'),
+                'minute': 0,
+                'second': 0,
+                'millisecond': 0
+            });
+
+            let newFechaFin = Cypress.moment().set({
+                'year': newDate.year(),
+                'month': newDate.month(),
+                'date': newDate.date(),
+                'hour': Cypress.moment().add(horaFinOffset, 'hours').format('HH'),
+                'minute': 0,
+                'second': 0,
+                'millisecond': 0
+            });
+            agenda.bloques[0].horaInicio = newFechaInicio;
+            agenda.bloques[0].horaFin = newFechaFin;
+            if (!agenda.dinamica) {
+                agenda.bloques[0].turnos[0].horaInicio = newFechaInicio;
+            }
+            agenda.fecha = newDate;
+            agenda.horaInicio = newFechaInicio;
+            agenda.horaFin = newFechaFin;
+
+            if (agenda.bloques && agenda.bloques.length && agenda.bloques[0].turnos && agenda.bloques[0].turnos.length) {
+                agenda.bloques[0].turnos[0].horaInicio = newFechaInicio;
+            }
+            if (agenda.sobreturnos && agenda.sobreturnos.length) {
+                agenda.sobreturnos[0].horaInicio = newFechaInicio;
+            }
+        } else { // no modifica la hora del json de la agenda, solo las fechas
+            let newDate = Cypress.moment().add(daysOffset, 'days').format('YYYY-MM-DD');
+            agenda.bloques[0].horaInicio = agenda.bloques[0].horaInicio.replace('2019-07-01', newDate);
+            agenda.bloques[0].horaFin = agenda.bloques[0].horaFin.replace('2019-07-01', newDate);
+            if (!agenda.dinamica) {
+                agenda.bloques[0].turnos[0].horaInicio = agenda.bloques[0].turnos[0].horaInicio.replace('2019-07-01', newDate);
+            }
+            agenda.fecha = agenda.fecha.replace('2019-07-01', newDate);
+            agenda.horaInicio = agenda.horaInicio.replace('2019-07-01', newDate);
+            agenda.horaFin = agenda.horaFin.replace('2019-07-01', newDate);
+        }
+        cy.request({
+            method: 'POST',
+            url: Cypress.env('API_SERVER') + '/api/modules/turnos/agenda',
+            body: agenda,
+            failOnStatusCode: false,
+            headers: {
+                Authorization: `JWT ${token}`
+            }
+        })
+    });
+});
+
 Cypress.Commands.add('createSolicitud', (name, token) => {
     return cy.fixture(name).then((solicitud) => {
         let newDate = Cypress.moment().format('YYYY-MM-DD');
