87071473ff67ab9e7a87e4dc152dc8c39d1083b4
Andrés <andresin@gmail.com>
2019-10-28 09:41:50 -0300

    feat(citas): test revisión de agendas (#35)

---
+++ cypress/integration/apps/citas/agendas/revision-agenda/revision_agendas.spec.js
@@ -0,0 +1,102 @@
+/// <reference types="Cypress" />
+
+export function buscarPaciente(pacienteDoc) {
+    cy.plexButton("Buscar Paciente").click();
+    cy.plexText('name="buscador"', pacienteDoc);
+
+    cy.server();
+    cy.route('GET', '**/api/core/mpi/pacientes**').as('listaPacientes');
+
+    cy.wait('@listaPacientes');
+
+    cy.get('tr td').contains(pacienteDoc).click();
+    cy.wait(100);
+
+    cy.plexButton("Cambiar Paciente").click();
+    cy.plexText('name="buscador"', pacienteDoc);
+
+    cy.wait('@listaPacientes');
+    cy.get('tr td').contains(pacienteDoc).click();
+};
+
+context('CITAS - Revisión de Agendas', () => {
+    let token;
+    let idAgenda;
+    let idBloque;
+    let idTurno;
+    let paciente;
+    let pacienteDoc;
+    before(() => {
+        cy.seed();
+
+        cy.viewport(1280, 720);
+        cy.login('30643636', 'asd').then(t => {
+            token = t;
+            return cy.createPaciente('paciente-masculino', token);
+        }).then(xhr => {
+            paciente = xhr.body;
+            pacienteDoc = xhr.body.documento;
+            return cy.createAgenda('agenda-auditada', null, null, null, token);
+        }).then((xhr) => {
+            idAgenda = xhr.body.id;
+            idBloque = xhr.body.bloques[0].id;
+            idTurno = xhr.body.bloques[0].turnos[1].id;
+            return cy.createTurno('nuevo-turno-asistio', idTurno, idBloque, idAgenda, paciente, token);
+            // }).then(xhr => {
+            // return cy.createPrestacionAgenda('prestacion-validada-turno', idTurno, paciente, token);
+        }).then(xhr => {
+            cy.log(xhr.body.paciente);
+        });
+    })
+
+    beforeEach(() => {
+        cy.goto(`/citas/revision_agenda/${idAgenda}`, token);
+        cy.server();
+    });
+
+    it('Comprueba datos de la agenda', () => {
+        cy.plexButtonIcon('chevron-down').click();
+        cy.get('plex-layout-sidebar header > fieldset > div > div:nth-child(2) > div').contains(/[A-Z]{3}\. [0-9]{2}\/[0-9]{2}\/[0-9]{4}, [0-9]{2}:[0-9]{2} a [0-9]{2}:[0-9]{2} hs/).should('not.be.empty');
+        cy.get('plex-layout-sidebar header > fieldset > div > div:nth-child(3) > div div').should('not.be.empty');
+        cy.get('plex-layout-sidebar header > fieldset > div > div:nth-child(4) > div div').should('not.be.empty');
+        cy.get('plex-layout-sidebar header > fieldset > div > div:nth-child(5) > div span').should('not.be.empty');
+        cy.plexButtonIcon('chevron-up').click();
+    });
+
+    it('Agregar y eliminar un diagnóstico', () => {
+        cy.get('tr:nth-child(1) td:first-child').click();
+
+        buscarPaciente(pacienteDoc);
+        cy.plexSelectType('label="Asistencia"', 'Asistio');
+
+        cy.plexText('name="searchTerm"', 'c11');
+
+        cy.route('GET', '**/api/core/term/cie10**').as('diagnosticos');
+
+        cy.wait('@diagnosticos');
+
+        cy.get('tr td').contains('C11.0').click();
+
+        cy.plexButtonIcon('delete').click();
+
+    });
+
+
+    it('Asignar asistencia a todos los turnos, mismo paciente', () => {
+
+        let listaTurnos = cy.get('plex-layout-sidebar > .plex-box > .plex-box-content table:first-child tr');
+
+        listaTurnos.each(($el, index, $list) => {
+            if (index > 0) {
+                cy.get($el).click();
+                buscarPaciente(pacienteDoc);
+                cy.plexSelectType('label="Asistencia"').click().get('.option').contains('No Asistio').click();
+                cy.get('.simple-notification').click();
+            }
+        });
+
+    });
+
+
+
+})
---
+++ cypress/integration/apps/citas/agendas/revision-agenda/revision_agendas_dos_prestaciones.spec.js
@@ -0,0 +1,53 @@
+/// <reference types="Cypress" />
+
+// import { buscarPaciente } from './revision_agendas.spec';
+
+context('CITAS - Revisión de Agendas', () => {
+    let token;
+    let horaInicio;
+    let tipoPrestacion1;
+    let tipoPrestacion2;
+    let idAgenda;
+    let idBloque;
+    let idTurno;
+    let paciente;
+    let pacienteDoc;
+
+    beforeEach(() => {
+        cy.seed();
+        cy.viewport(1280, 720);
+        cy.login('30643636', 'asd').then(t => {
+            token = t;
+            return cy.createPaciente('paciente-masculino', token);
+        }).then(xhr => {
+            paciente = xhr.body;
+            pacienteDoc = xhr.body.documento;
+            return cy.createAgenda('agenda-auditada-con-dos-prestaciones', null, null, null, token);
+        }).then((xhr) => {
+            idAgenda = xhr.body.id;
+            idBloque = xhr.body.bloques[0].id;
+            idTurno = xhr.body.bloques[0].turnos[1].id;
+            horaInicio = xhr.body.horaInicio;
+            tipoPrestacion1 = xhr.body.tipoPrestaciones[0];
+            tipoPrestacion2 = xhr.body.tipoPrestaciones[1];
+        }).then(xhr => {
+            cy.log(xhr.body.paciente);
+        });
+    });
+
+
+    it.skip('Se selecciona la primera de dos prestaciones, luego se cambia por la segunda', () => {
+        cy.goto(`/citas/revision_agenda/${idAgenda}`, token);
+        cy.get('tbody:nth-child(1) tr:nth-child(3)').click();
+
+        cy.buscarPaciente(pacienteDoc, false);
+
+        // El <plex-select> no está armado con label
+        cy.plexSelectType('name="tipoPrestacionTurno"').click().get('.option').contains(tipoPrestacion1.term).click();
+        cy.wait(1000);
+        cy.plexSelectType('name="tipoPrestacionTurno"').click().get('.option').contains(tipoPrestacion2.term).click();
+        cy.plexSelectType('label="Asistencia"', 'Asistio');
+
+    });
+
+})
