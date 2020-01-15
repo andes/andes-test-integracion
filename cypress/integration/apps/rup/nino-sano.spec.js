c108cd4e5af680ff46247a886ae16780d8c5672c
plammel <lammelpablo@gmail.com>
2020-01-15 12:16:10 -0300

    feat(RUP): Registra ruta de CDA de paciente y remueve fixrure de frecuentes paciente

--- cypress/integration/apps/rup/nino-sano.spec.js
+++ cypress/integration/apps/rup/nino-sano.spec.js
@@ -18,12 +18,8 @@ context('RUP - Punto de inicio', () => {
         cy.goto('/rup', token);
 
         cy.server();
-        const fixtures = [];
-        cy.fixture('apps/rup/nino-sano.json').then(json => {
-            fixtures.push(json);
-        });
         // Stub
-        cy.route(/api\/modules\/rup\/frecuentesProfesional\?/, fixtures).as('search');
+        cy.route(/api\/modules\/rup\/frecuentesProfesional\?/).as('search');
         cy.route('GET', '/api/modules/rup/prestaciones/huds/**', []).as('huds');
         cy.route('GET', '**api/core/tm/tiposPrestaciones**').as('prestaciones');
         cy.route('POST', '**/api/modules/rup/prestaciones').as('create');
@@ -32,6 +28,7 @@ context('RUP - Punto de inicio', () => {
         cy.route('PATCH', 'api/modules/rup/prestaciones/**').as('patch');
         cy.route('GET', '/api/core/term/snomed/expression**', []).as('expression');
         cy.route('POST', '/api/modules/rup/codificacion').as('codificacion');
+        cy.route('GET', '/api/modules/cda/paciente/**').as('cdaPaciente');
 
         cy.plexButton('PACIENTE FUERA DE AGENDA').click();
 
@@ -52,48 +49,52 @@ context('RUP - Punto de inicio', () => {
             expect(xhr.response.body.paciente.documento).to.be.eq('3399661');
             expect(xhr.response.body.estados[0].tipo).to.be.eq('ejecucion');
         });
-        cy.plexText('name="search"', 'consulta de niño sano, recién nacido');
-        cy.wait('@search').then((xhr) => {
-            cy.get('.mdi-plus').first().click();
-        });
 
-        cy.plexFloat('label="Peso"', 63);
-        cy.plexFloat('label="Percentilo de Peso"', 70);
-        cy.plexFloat('name="talla"', 120);
-        cy.plexFloat('label="Percentilo de Talla"', 80);
-        cy.plexFloat('label="Perímetro Cefálico"', 55);
-        cy.plexFloat('label="Percentilo de Perímetro Cefálico"', 40);
-
-        cy.get('plex-bool').first().click();
-
-        cy.plexButton('Guardar consulta de niño sano').click();
-
-        cy.wait('@patch').then((xhr) => {
+        cy.wait('@cdaPaciente').then((xhr) => {
             expect(xhr.status).to.be.eq(200);
-            expect(xhr.response.body.solicitud.turno).to.be.undefined;
-            expect(xhr.response.body.solicitud.tipoPrestacion.id).to.be.eq('57f5069d69fe79a5980b072f');
-            expect(xhr.response.body.paciente.documento).to.be.eq('3399661');
-            expect(xhr.response.body.estados[0].tipo).to.be.eq('ejecucion');
-            expect(xhr.response.body.estados[1]).to.be.eq(undefined);
-        });
-
-        cy.toast('success');
-        cy.plexButton('Validar consulta de niño sano').click();
-
-        // Popup alert
-        cy.get('button').contains('CONFIRMAR').click();
-
-        cy.wait('@patch').then((xhr) => {
-            expect(xhr.status).to.be.eq(200);
-            expect(xhr.response.body.solicitud.turno).to.be.undefined;
-            expect(xhr.response.body.solicitud.tipoPrestacion.id).to.be.eq('57f5069d69fe79a5980b072f');
-            expect(xhr.response.body.paciente.documento).to.be.eq('3399661');
-            expect(xhr.response.body.estados[1].tipo).to.be.eq('validada');
-            expect(xhr.response.body.estados[2]).to.be.eq(undefined);
-        });
-        cy.wait('@codificacion').then((xhr) => {
-            expect(xhr.status).to.be.eq(200);
-
+            cy.plexText('name="search"', 'consulta de niño sano, recién nacido');
+            cy.wait('@search').then((xhr) => {
+                cy.get('.mdi-plus').first().click();
+            });
+    
+            cy.plexFloat('label="Peso"', 63);
+            cy.plexFloat('label="Percentilo de Peso"', 70);
+            cy.plexFloat('name="talla"', 120);
+            cy.plexFloat('label="Percentilo de Talla"', 80);
+            cy.plexFloat('label="Perímetro Cefálico"', 55);
+            cy.plexFloat('label="Percentilo de Perímetro Cefálico"', 40);
+    
+            cy.get('plex-bool').first().click();
+    
+            cy.plexButton('Guardar consulta de niño sano').click();
+    
+            cy.wait('@patch').then((xhr) => {
+                expect(xhr.status).to.be.eq(200);
+                expect(xhr.response.body.solicitud.turno).to.be.undefined;
+                expect(xhr.response.body.solicitud.tipoPrestacion.id).to.be.eq('57f5069d69fe79a5980b072f');
+                expect(xhr.response.body.paciente.documento).to.be.eq('3399661');
+                expect(xhr.response.body.estados[0].tipo).to.be.eq('ejecucion');
+                expect(xhr.response.body.estados[1]).to.be.eq(undefined);
+            });
+            
+            cy.toast('success');
+            cy.plexButton('Validar consulta de niño sano').click();
+    
+            // Popup alert
+            cy.get('button').contains('CONFIRMAR').click();
+    
+            cy.wait('@patch').then((xhr) => {
+                expect(xhr.status).to.be.eq(200);
+                expect(xhr.response.body.solicitud.turno).to.be.undefined;
+                expect(xhr.response.body.solicitud.tipoPrestacion.id).to.be.eq('57f5069d69fe79a5980b072f');
+                expect(xhr.response.body.paciente.documento).to.be.eq('3399661');
+                expect(xhr.response.body.estados[1].tipo).to.be.eq('validada');
+                expect(xhr.response.body.estados[2]).to.be.eq(undefined);
+            });
+            cy.wait('@codificacion').then((xhr) => {
+                expect(xhr.status).to.be.eq(200);
+    
+            });
         });
 
     });
