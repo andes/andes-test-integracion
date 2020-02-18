c61f0f042962a6b7c3b41645afbffc0706e7ca7d
Mariano Andres Botta <marianoabotta@gmail.com>
2020-02-18 09:13:01 -0300

    feat(elemento-rup): testing de selects parametrizables

---
+++ cypress/integration/apps/rup/elementos-rup/atomo-select.spec.js
@@ -0,0 +1,87 @@
+/// <reference types="Cypress" />
+
+context('RUP - Ejecucion', () => {
+    let token;
+
+    before(() => {
+        cy.seed();
+        cy.login('30643636', 'asd').then(t => {
+            token = t;
+        });
+        cy.task('database:seed:paciente');
+    });
+
+
+    describe('select organizacion', () => {
+        let idPrestacion, idElementoRUP;
+        beforeEach(() => {
+            cy.server();
+            cy.route('GET', '/api/modules/rup/prestaciones/huds/**', []).as('huds');
+            cy.route('PATCH', '/api/modules/rup/prestaciones/**').as('patchPrestacion');
+            cy.route('GET', '/api/core/term/snomed?search=derivaci&semanticTag=hallazgo&semanticTag=trastorno&semanticTag=procedimiento&semanticTag=entidad observable&semanticTag=producto&semanticTag=situación&semanticTag=régimen/tratamiento&semanticTag=elemento de registro&semanticTag=objeto físico&semanticTag=medicamento clínico&semanticTag=fármaco de uso clínico&semanticTag=evento', [{
+                "conceptId": "440377005",
+                "term": "derivación por",
+                "fsn": "derivación por (entidad observable)",
+                "semanticTag": "entidad observable"
+            }]).as('rup-buscador');
+
+            cy.cleanDB(['prestaciones']);
+
+            cy.task('database:seed:elemento-rup', {
+                componente: 'SelectOrganizacionComponent',
+                params: {
+                    title: 'ORGANIZACION CUSTOM',
+                    required: true,
+                }
+            }).then((elementoRup) => idElementoRUP = elementoRup._id);
+
+            cy.task('database:seed:prestacion', { paciente: '586e6e8627d3107fde116cdb', tipoPrestacion: '5cdc4c865cd661b503d727a6' }).then((prestacion) => {
+                idPrestacion = prestacion._id;
+                cy.goto('/rup/ejecucion/' + idPrestacion, token);
+            });
+
+        })
+
+        afterEach(() => {
+            cy.task('database:delete:elemento-rup', idElementoRUP);
+        })
+
+
+        it('test validacion y grabar', () => {
+
+            cy.route('GET', '**/api/core/tm/organizaciones**').as('getOrganizaciones');
+            cy.route('GET', '**/api/modules/cda/paciente/**').as('paciente');
+
+            cy.get('rup-buscador button').contains('BUSCADOR BÁSICO ').click();
+            cy.get('snomed-buscar').plexText('name="searchTerm"', 'derivaci');
+            cy.wait('@rup-buscador');
+            cy.get('rup-buscador .mdi-plus').first().click();
+
+            cy.get('.rup-card').first().as('rupCard');
+
+            cy.plexButton('Guardar').click();
+            cy.toast('error');
+
+            cy.get('@rupCard').contains('ORGANIZACION CUSTOM');
+            cy.get('@rupCard').plexSelectAsync('name="organizacion"', 'castro', '@getOrganizaciones', 0);
+
+            cy.plexButton('Guardar').click();
+            cy.wait('@patchPrestacion');
+
+            cy.url().should('include', '/rup/validacion/');
+
+            cy.get('.rup-card').first().contains('ORGANIZACION CUSTOM');
+            cy.get('.rup-card').first().contains('PUESTO SANITARIO RAMON CASTRO (POR NOMIVAC)')
+
+            cy.plexButton('Continuar').click();
+
+            cy.wait('@paciente');
+            cy.plexButtonIcon('chevron-down').click();
+
+            cy.get('.rup-card').first().as('rupCard');
+            cy.get('@rupCard').plexSelect('name="organizacion"').contains('PUESTO SANITARIO RAMON CASTRO (POR NOMIVAC)');
+
+        });
+
+    })
+})
