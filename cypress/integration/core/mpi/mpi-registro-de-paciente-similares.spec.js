ffb0c19a39f7f6f4eec60dbdf4b9ee78c67f1cbf
plammel <lammelpablo@gmail.com>
2020-04-21 10:10:56 -0300

    MPI: Matcheo de pacientes similares (#142)
    * feat(MPI): Matcheo de pacientes similares
    
    * fix(MPI): agrega spec al nombre del archivo
    
    Co-authored-by: palita1991 <marianopalavecino7@gmail.com>

---
+++ cypress/integration/core/mpi/mpi-registro-de-paciente-similares.spec.js
@@ -0,0 +1,150 @@
+context('MPI-Registro de Pacientes Similares', () => {
+    let token
+    before(() => {
+        cy.seed();
+        cy.cleanDB()
+        cy.task('database:seed:paciente');
+        cy.login('38906735', 'asd').then(t => {
+            token = t;
+        });
+    })
+
+    beforeEach(() => {
+        cy.log(token)
+        cy.goto('/apps/mpi/busqueda', token);
+        cy.server();
+
+        // Intercepta la llamada a la ruta validar y devuelve paciente_validado
+        cy.route('POST', '**api/core/mpi/pacientes**').as('postPaciente');
+        cy.route('PUT', '**api/core/mpi/pacientes/**').as('putPaciente');
+        cy.route('GET', '**api/core/mpi/pacientes/**').as('getPaciente');
+    })
+    
+    function irANuevoPaciente() {
+        // Buscador
+        cy.plexText('name="buscador"', 'xxx');
+        cy.get('div').contains('NUEVO PACIENTE').click();
+        cy.get('div').contains('CON DNI ARGENTINO').click();
+    }
+
+    it('Match >94%, dni repetido', () => {
+
+        irANuevoPaciente();
+        // Se completa datos básicos
+        cy.plexInt('name="documento"', '10000000');
+        cy.plexDatetime('name="fechaNacimiento"', '26/12/1956');
+        cy.plexSelectType('name="sexo"', 'Masculino');
+        cy.plexText('name="apellido"', 'ANDES');
+        cy.plexText('name="nombre"', 'VALIDADO');
+        cy.plexPhone('label="Número"', '2990000000');
+        cy.plexBool('label="No posee ningún tipo de contacto"', true);
+        cy.plexBool('name="viveProvActual"', true);
+        cy.plexBool('name="viveLocActual"', true);
+        cy.plexButton('Guardar').click();
+        
+        cy.wait('@postPaciente').then((xhr) => {
+            expect(xhr.status).to.be.eq(200);
+            expect(xhr.response.body.resultadoMatching.length).to.be.eq(1);
+            expect(xhr.response.body.resultadoMatching[0].match).to.be.eq(0.96);
+            expect(xhr.response.body.resultadoMatching[0].paciente._id).to.be.eq('586e6e8627d3107fde116cdb');
+            expect(xhr.response.body.macheoAlto).to.be.eq(true);
+            expect(xhr.response.body.dniRepetido).to.be.eq(true);
+        });
+        // Popup alert
+    
+        cy.swal('confirm');
+        cy.plexButton('Guardar').should('have.prop', 'disabled', true);
+        cy.plexButton('Seleccionar').click();
+        cy.plexButton('Guardar').click();
+        cy.wait('@putPaciente').then((xhr) => {
+            expect(xhr.status).to.be.eq(200);
+        });
+    });
+
+    it('Match <94%, dni repetido, selecciona match', () => {
+
+        irANuevoPaciente();
+        // Se completa datos básicos
+        cy.plexInt('name="documento"', '10000000');
+        cy.plexDatetime('name="fechaNacimiento"', Cypress.moment().format('DD/MM/YYYY'));
+        cy.plexSelectType('name="sexo"', 'Masculino');
+        cy.plexText('name="apellido"', 'X');
+        cy.plexText('name="nombre"', 'X');
+        cy.plexPhone('label="Número"', '2999999999');
+        cy.plexBool('label="No posee ningún tipo de contacto"', true);
+        cy.plexSelectType('name="provincia"', 'CABA');
+        cy.plexButton('Guardar').click();
+        
+        cy.wait('@postPaciente').then((xhr) => {
+            expect(xhr.status).to.be.eq(200);
+            expect(xhr.response.body.resultadoMatching.length).to.be.eq(1);
+            expect(xhr.response.body.resultadoMatching[0].match).to.be.eq(0.87);
+            expect(xhr.response.body.resultadoMatching[0].paciente._id).to.be.eq('586e6e8627d3107fde116cdb');
+            expect(xhr.response.body.macheoAlto).to.be.eq(false);
+            expect(xhr.response.body.dniRepetido).to.be.eq(true);
+        });
+        // Popup alert
+    
+        cy.swal('confirm');
+        cy.plexButton('Seleccionar').click();
+        cy.plexButton('Guardar').click();
+        cy.wait('@putPaciente').then((xhr) => {
+            expect(xhr.status).to.be.eq(200);
+        });
+    });
+
+    it('Match <94%, dni distinto, ignora match', () => {
+
+        irANuevoPaciente();
+        // Se completa datos básicos
+        cy.plexInt('name="documento"', '10000001');
+        cy.plexDatetime('name="fechaNacimiento"', '26/12/1956');
+        cy.plexSelectType('name="sexo"', 'Masculino');
+        cy.plexText('name="apellido"', 'ANDES');
+        cy.plexText('name="nombre"', 'PACIENTE VALIDADO');
+        cy.plexPhone('label="Número"', '2994545454');
+        cy.plexBool('label="No posee ningún tipo de contacto"', true);
+        cy.plexSelectType('name="provincia"', 'CABA');
+        cy.plexButton('Guardar').click();
+        
+        cy.wait('@postPaciente').then((xhr) => {
+            expect(xhr.status).to.be.eq(200);
+            expect(xhr.response.body.resultadoMatching.length).to.be.eq(1);
+            expect(xhr.response.body.resultadoMatching[0].match).to.be.eq(0.93);
+            expect(xhr.response.body.resultadoMatching[0].paciente._id).to.be.eq('586e6e8627d3107fde116cdb');
+            expect(xhr.response.body.macheoAlto).to.be.eq(false);
+            expect(xhr.response.body.dniRepetido).to.be.eq(false);
+        });
+        // Popup alert
+    
+        cy.swal('confirm');
+        cy.plexButton('Ignorar y Guardar').click();
+        cy.wait('@postPaciente').then((xhr) => {
+            expect(xhr.status).to.be.eq(200);
+        });
+    });
+
+    it('Paciente similar temporal: no arroja matches', () => {
+
+        // Buscador
+        irANuevoPaciente();
+        // Se completa datos básicos
+        cy.plexInt('name="documento"', '20000000');
+        cy.plexDatetime('name="fechaNacimiento"', '26/12/1990');
+        cy.plexSelectType('name="sexo"', 'Femenino');
+        cy.plexText('name="apellido"', 'ANDES');
+        cy.plexText('name="nombre"', 'PACIENTE TEMPORAL');
+        cy.plexPhone('label="Número"', '2990000000');
+        cy.plexBool('label="No posee ningún tipo de contacto"', true);
+        cy.plexBool('name="viveProvActual"', true);
+        cy.plexBool('name="viveLocActual"', true);
+        cy.plexButton('Guardar').click();
+        cy.wait('@postPaciente').then((xhr) => {
+            expect(xhr.status).to.be.eq(200);
+            expect(xhr.response.body.resultadoMatching).to.be.eq(undefined);
+        });
+        // Popup alert
+        cy.swal('confirm');
+    });
+
+});
