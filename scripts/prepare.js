834b53c99a07607f592e7002a05ab2492366eac2
Mariano Andres Botta <marianoabotta@gmail.com>
2020-07-31 10:30:55 -0300

    feat: quita elementos de la db

--- scripts/prepare.js
+++ scripts/prepare.js
@@ -4,27 +4,16 @@ const type = process.argv[2] || 'develop';
 const up = {
     production: [
         'npm run prod:down',
-        'npx cross-env APP=master API=master docker-compose -f docker/docker-compose.yml up -d',
-        'node ./scripts/prepare.js production sleep',
-        'docker exec andes_db mongo --eval "rs.initiate();"',
-        // 'curl -XPUT "http://localhost:9200/andes/" -d @docker/andes-index.json',
-        // 'docker cp docker/andes.gz andes_db:/andes.gz',
-        // 'docker exec andes_db mongorestore --gzip --archive=/andes.gz --db andes',
+        'npx cross-env APP=master API=master docker-compose -f docker/docker-compose-prod-local.yml up -d',
     ],
     develop: [
         'docker-compose -f docker/docker-local.yml up -d',
-        'node ./scripts/prepare.js develop sleep',
-        'docker exec andes_db mongo --eval "rs.initiate();"',
-        // 'mongo andes --eval "db.getCollectionNames().forEach(function(n){db[n].remove()});"',
-        // 'curl -XDELETE "http://localhost:9200/andes"',
-        // 'curl -XPUT "http://localhost:9200/andes/" -d @docker/andes-index.json',
-        // 'mongorestore --gzip --archive=./docker/andes.gz --db andes'
     ]
 }
 
 const down = {
     production: [
-        'docker-compose -f docker/docker-compose.yml down -v'
+        'docker-compose -f docker/docker-compose-prod-local.yml down -v'
     ],
     develop: [
         'docker-compose -f docker/docker-local.yml down -v'
@@ -33,9 +22,9 @@ const down = {
 
 const reset = {
     production: [
-        'docker exec andes_db mongo andes --eval "db.getCollectionNames().forEach(function(n){db[n].remove({})});"',
-        'docker cp docker/andes.gz andes_db:/andes.gz',
-        'docker exec andes_db mongorestore --gzip --archive=/andes.gz --db andes',
+        'docker exec andes_test_db mongo andes --eval "db.getCollectionNames().forEach(function(n){db[n].remove({})});"',
+        'docker cp docker/andes.gz andes_test_db:/andes.gz',
+        'docker exec andes_test_db mongorestore --gzip --archive=/andes.gz --db andes',
     ],
     develop: [
         'docker exec andes_db mongo andes --eval "db.getCollectionNames().forEach(function(n){db[n].remove({})});"',
@@ -46,16 +35,8 @@ const reset = {
 
 const backup = {
     production: [
-        'docker exec andes_db mongodump --gzip --archive=/andes.gz --db andes',
-        'docker cp andes_db:/andes.gz docker/andes.gz'
-    ]
-};
-
-const cleanup = {
-    production: [
-        'docker exec andes_db mongo andes --eval "db.paciente.remove();db.agenda.remove();db.prestaciones.remove();"',
-        'curl -XDELETE "http://localhost:9266/andes"',
-        'curl -XPUT "http://localhost:9266/andes/" -d @docker/andes-index.json'
+        'docker exec andes_test_db mongodump --gzip --archive=/andes.gz --db andes',
+        'docker cp andes_test_db:/andes.gz docker/andes.gz'
     ]
 };
 
@@ -82,9 +63,6 @@ switch (action) {
         break;
     case 'backup':
         runCommands(backup[type]);
-        break;
-    case 'cleanup':
-        runCommands(cleanup[type]);
         break;
     case 'sleep':
         sleep();
