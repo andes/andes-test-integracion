0e3898ae2b77097be4bc6e3c025ce3c42a07cdcb
Mariano Andres Botta <marianoabotta@gmail.com>
2020-08-27 11:02:59 -0300

    fix(mapa-camas): varios errores en los test

--- scripts/seeder.js
+++ scripts/seeder.js
@@ -1,12 +1,28 @@
+const path = require('path');
+const { cleanDB, connectToDB } = require('../cypress/plugins/database');
 const { Seeder } = require('mongo-seeding');
+
+
 const config = {
     database: process.env.MONGO_URI || 'mongodb://localhost:27066/andes',
     dropDatabase: false,
 };
-const seeder = new Seeder(config);
-const path = require('path');
-const collections = seeder.readCollectionsFromPath(path.resolve("./data"));
-seeder.import(collections).then(() => process.exit());
+
+
+async function main() {
+    // Clean DATABASE
+    const client = await connectToDB(config.database);
+    await cleanDB(client.db());
+
+    // SEED DATABASE
+    const seeder = new Seeder(config);
+    const collections = seeder.readCollectionsFromPath(path.resolve("./data"));
+    await seeder.import(collections)
+
+    process.exit();
+}
+main();
+
 
 
 
