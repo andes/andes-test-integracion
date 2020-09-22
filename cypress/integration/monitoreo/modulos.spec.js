0ee06bf3751c7b366b546b24103b6a69e43c05ce
Andrés Boc-Ho <andresin@gmail.com>
2020-09-22 11:01:57 -0300

    feat(monitoreo): tests de submódulos

--- cypress/integration/monitoreo/modulos.spec.js
+++ cypress/integration/monitoreo/modulos.spec.js
@@ -19,15 +19,13 @@ context('Gestor de módulos', () => {
     });
 
     it('Registro de nuevo módulo', () => {
-        let text = 'alguna registro universal de prestaciones de pacientes';
         cy.plexButton('Nuevo módulo').click();
-        cy.plexBool('name="activo"', true); // `Inactiva`
-        cy.plexText('label="nombre"', 'HUDS');
-        cy.plexText('label="subtitulo"', 'historia digital de salud');
-        cy.plexText('label="descripcion"', 'registro universal de prestaciones de pacientes');
-        cy.plexText('label="Link"', '/rup');
-        cy.plexText('label="Icono"', 'mdi-contacts');
-        cy.plexInt('label="Orden"', '1');
+        cy.plexBool('formControlName="activo"', true); // `Inactiva`
+        cy.plexText('formControlName="nombre"', 'RUP');
+        cy.plexText('formControlName="subtitulo"', 'Registro Universal de Prestaciones');
+        cy.plexText('formControlName="descripcion"', 'Ambulatorio');
+        cy.plexText('formControlName="linkAcceso"', '/rup');
+        cy.plexInt('formControlName="orden"', '1');
         cy.plexButton('Guardar').click();
         cy.wait('@postModulos').then((xhr) => {
             expect(xhr.status).to.be.eq(200);
@@ -40,15 +38,34 @@ context('Gestor de módulos', () => {
         cy.get('plex-item').contains('Activo');
     });
 
-    it('Carga de módulo como INACTIVO y edición para activarlo', () => {
-        cy.plexButton('Nuevo módulo').click();
-        cy.plexBool('name="activo"', false);
-        cy.plexText('label="nombre"', 'DASHBOARD');
-        cy.plexText('label="subtitulo"', 'Estadísticas');
-        cy.plexText('label="descripcion"', 'Visualice datos estadísticos en gráficos y tablas');
-        cy.plexText('label="Link"', '/dashboard');
-        cy.plexText('label="Icono"', 'mdi-chart-line');
-        cy.plexInt('label="Orden"', '2');
+    it('Registro de módulo con 1 submódulo', () => {
+        cy.plexButton('Nuevo módulo').click().blur();
+        cy.plexBool('formControlName="activo"', true); // `Inactiva`
+
+        cy.get('.text-warning').should('contain', 'Hay campos requeridos sin completar');
+
+        cy.plexText('formControlName="nombre"', 'Módulo Principal');
+        cy.plexText('formControlName="subtitulo"', 'Módulo con submódulo');
+        cy.plexText('formControlName="descripcion"', 'Principal');
+        cy.plexText('formControlName="linkAcceso"', '/principal');
+        cy.plexText('formControlName="icono"', 'cat');
+        cy.plexInt('formControlName="orden"', 1);
+
+        // Submódulos
+        cy.plexButton('Agregar submódulo').click();
+
+        cy.get('.submodulos').plexBool('formControlName="activo"', true); // `Inactiva`
+        cy.get('.submodulos').plexText('formControlName="nombre"', 'Módulo Secundario');
+        cy.get('.submodulos').plexText('formControlName="linkAcceso"', '/principal/secundario');
+        cy.get('.submodulos input[type="color"]')
+            .focus()
+            .invoke('val', '#ff9966')
+            .trigger('change')
+        cy.get('.submodulos').plexText('formControlName="icono"', 'panda');
+        cy.get('.submodulos').plexInt('formControlName="orden"', '1');
+
+        cy.get('.text-warning').should('not.be.visible');
+
         cy.plexButton('Guardar').click();
         cy.wait('@postModulos').then((xhr) => {
             expect(xhr.status).to.be.eq(200);
@@ -57,23 +74,45 @@ context('Gestor de módulos', () => {
         cy.wait('@modulos').then((xhr) => {
             expect(xhr.status).to.be.eq(200);
         });
-        cy.get('plex-item').plexBadge('Inactivo').first().click();
-        cy.plexBool('label="Activo"', true);
+        cy.get('plex-item').contains('Módulo Principal');
+        cy.get('plex-item').contains('Activo');
+    });
+
+    it('Carga de módulo como INACTIVO y edición para activarlo', () => {
+        cy.plexButton('Nuevo módulo').click();
+        cy.plexBool('formControlName="activo"', false);
+        cy.plexText('formControlName="nombre"', 'DASHBOARD');
+        cy.plexText('formControlName="subtitulo"', 'Estadísticas');
+        cy.plexText('formControlName="descripcion"', 'Visualice datos estadísticos en gráficos y tablas');
+        cy.plexText('formControlName="linkAcceso"', '/dashboard');
+        cy.plexText('formControlName="icono"', 'chart-line');
+        cy.plexInt('formControlName="orden"', '2');
         cy.plexButton('Guardar').click();
-        cy.plexText('label="Nombre"', 'DASHBOARD');
+        cy.wait('@postModulos').then((xhr) => {
+            expect(xhr.status).to.be.eq(200);
+        });
+        cy.toast('success', 'Los datos se guardaron correctamente');
+        cy.wait('@modulos').then((xhr) => {
+            expect(xhr.status).to.be.eq(200);
+        });
+
+        cy.get('plex-item').plexBadge('Inactivo').first().click();
+
+        cy.plexBool('formControlName="activo"', true);
+        cy.plexButton('Guardar').click();
         cy.get('plex-item').first().contains('Activo');
 
     })
 
     it('Buscar un módulo por título ', () => {
         cy.plexButton('Nuevo módulo').click();
-        cy.plexBool('name="activo"', true); // `Inactiva`
-        cy.plexText('label="nombre"', 'INTERNACIÓN');
-        cy.plexText('label="subtitulo"', 'Mapa de camas');
-        cy.plexText('label="descripcion"', 'Vea el estado del mapa de camas de los distintos servicios de la organización');
-        cy.plexText('label="Link"', '/internacion/mapa-de-camas');
-        cy.plexText('label="Icono"', 'mdi-bed');
-        cy.plexInt('label="Orden"', '3');
+        cy.plexBool('formControlName="activo"', true); // `Inactiva`
+        cy.plexText('formControlName="nombre"', 'INTERNACIÓN');
+        cy.plexText('formControlName="subtitulo"', 'Mapa de camas');
+        cy.plexText('formControlName="descripcion"', 'Vea el estado del mapa de camas de los distintos servicios de la organización');
+        cy.plexText('formControlName="linkAcceso"', '/internacion/mapa-de-camas');
+        cy.plexText('formControlName="icono"', 'cama-paciente');
+        cy.plexInt('formControlName="orden"', '3');
         cy.plexButton('Guardar').click();
         cy.wait('@postModulos').then((xhr) => {
             expect(xhr.status).to.be.eq(200);
