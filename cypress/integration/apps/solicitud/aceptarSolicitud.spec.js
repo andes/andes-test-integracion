9d0344cac5b23b7ad8329ca08095f297ee3d6522
plammel <lammelpablo@gmail.com>
2020-10-28 12:14:20 -0300

    AM: test auditoria

---
+++ cypress/integration/apps/solicitud/aceptarSolicitud.spec.js
@@ -0,0 +1,43 @@
+/// <reference types="Cypress" />
+
+/**
+ * VER TEMA DE REGLAS COMO RESETEAR
+ * A LA NOCHE DA ERROR POR PROBLEMA DE TIMEZONE
+ */
+
+context('ACEPTAR SOLICITUD', () => {
+    let token
+    before(() => {
+        cy.seed();
+        cy.login('30643636', 'asd').then(t => {
+            token = t;
+            cy.createPaciente('solicitudes/paciente-solicitud', token);
+            cy.createPrestacion('solicitudes/solicitud-auditoria', token)
+        })
+    })
+
+     beforeEach(() => {
+        cy.goto('/solicitudes', token);
+        cy.server();
+        cy.route('GET', '**/api/modules/rup/prestaciones/solicitudes**').as('solicitudes');
+        cy.route('PATCH', '**/api/modules/rup/prestaciones/**').as('auditarSolicitud');
+    })
+
+
+
+    it('aceptar', () => {
+        cy.server();
+        cy.plexButtonIcon('lock-alert').first().click();
+        cy.plexButton('Aceptar').click();
+        cy.get('textarea').last().type('Una observacion aceptar', { force: true });
+        cy.plexButton('Confirmar').click();
+        cy.wait('@auditarSolicitud').then((xhr) => {
+            const i = xhr.response.body.solicitud.historial.length - 1;
+            expect(xhr.status).to.be.eq(200);
+            expect(xhr.response.body.solicitud.historial[i].descripcion).to.be.eq('Aceptada');
+            expect(xhr.response.body.solicitud.historial[i].observaciones).to.be.eq('Una observacion aceptar');  
+        });
+        cy.get('plex-badge').contains('pendiente');
+        cy.plexButtonIcon('calendar-plus');
+    })
+})
---
+++ cypress/integration/apps/solicitud/responderSolicitud.spec.js
@@ -0,0 +1,40 @@
+/// <reference types="Cypress" />
+
+context('RESPONDER SOLICITUD', () => {
+    let token;
+    before(() => {
+        cy.seed();
+        cy.login('30643636', 'asd').then(t => {
+            token = t;
+            cy.createPaciente('solicitudes/paciente-solicitud', token);
+            cy.createPrestacion('solicitudes/solicitud-auditoria', token)
+        });
+    });
+
+     beforeEach(() => {
+        cy.goto('/solicitudes', token);
+        cy.server();
+        cy.route('GET', '**/api/core/tm/profesionales**').as('getProfesional');
+        cy.route('GET', '**/api/modules/rup/prestaciones/solicitudes**').as('solicitudes');
+        cy.route('PATCH', '**/api/modules/rup/prestaciones/**').as('patchSolicitud');
+    });
+
+    it('responder', () => {
+        cy.server();
+        cy.plexButtonIcon('lock-alert').first().click();
+        cy.plexButton('Responder').click();
+        cy.get('textarea').last().type('No se acepta por ...', { force: true });
+        cy.plexButton('Confirmar').click();
+        cy.wait('@patchSolicitud').then((xhr) => {
+            const i = xhr.response.body.solicitud.historial.length - 1;
+            expect(xhr.status).to.be.eq(200);
+            expect(xhr.response.body.estados[1].observaciones).to.be.eq('No se acepta por ...');
+            expect(xhr.response.body.solicitud.historial[i].observaciones).to.be.eq('No se acepta por ...');
+        
+        });
+        cy.wait('@solicitudes').then((xhr) => {
+            expect(xhr.status).to.be.eq(200);
+        });
+        cy.get('plex-badge').contains('CONTRARREFERIDA');
+    });
+});
