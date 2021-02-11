d83dac28d6388f839a9a2ea4aa2c9200c9f97ccb
plammel <lammelpablo@gmail.com>
2021-02-11 07:35:57 -0300

    feat(COM): test traslado especial (#318)

--- cypress/integration/apps/com/com.spec.js
+++ cypress/integration/apps/com/com.spec.js
@@ -12,6 +12,7 @@ context('CENTRO OPERATIVO MÉDICO', () => {
             cy.task('database:create:paciente', { template: 'validado', nombre: 'PACIENTE', apellido: 'COM 3', documento: 2111893 });
             cy.task('database:create:paciente', { template: 'validado', nombre: 'PACIENTE', apellido: 'COM 4', documento: 2001294 });
             cy.task('database:create:paciente', { template: 'validado', nombre: 'PACIENTE', apellido: 'COM 5', documento: 2504195 });
+            cy.task('database:create:paciente', { template: 'validado', nombre: 'PACIENTE', apellido: 'COM 6', documento: 2504196 });
         })
     });
 
@@ -319,5 +320,115 @@ context('CENTRO OPERATIVO MÉDICO', () => {
         cy.get('plex-label').contains('Solicitante: PRUEBA, ALICIA').first().click();
         cy.get('plex-options div div button').contains('HISTORIAL').click({ force: true });
         cy.get('plex-panel').should('have.length', 5);
+    });
+
+    it('crear derivacion traslado especial', () => {
+        seleccionarPaciente('2504196');
+        cy.wait('@profesionalSolicitante').then((xhr) => {
+            expect(xhr.status).to.be.eq(200);
+        });
+        cy.wait('@getOrganizacion').then((xhr) => {
+            expect(xhr.status).to.be.eq(200);
+        });
+        cy.plexTextArea('label="Detalle"', 'a aceptar');
+        cy.plexSelectType('name="profesionalOrigen"').clearSelect();
+        cy.plexSelectAsync('label="Profesional solicitante"', 'ALICIA PRUEBA', '@profesionalSolicitante', 0);
+        cy.plexSelectType('name="trasladoEspecial"', 'VUELO SANITARIO');
+        cy.wait('@getOrganizaciones');
+        cy.plexSelectType('name="organizacionTraslado"', 1);
+
+        cy.plexButton("Guardar").click({ force: true });
+        cy.wait('@createDerivacion').then((xhr) => {
+            expect(xhr.status).to.be.eq(200);
+            expect(xhr.response.body.paciente.documento).to.be.eq('2504196');
+        });
+        cy.toast('success', 'Derivación guardada');
+
+        cy.login('30643636', 'asd', '5ef7ae883e3de57c0a429797').then(t => {
+            token = t;
+            cy.goto('/com', token);
+        });
+        cy.get('plex-label').contains('Solicitante: PRUEBA, ALICIA').should('have.length', 1);
+        cy.get('plex-item').last().click();
+        cy.get('small').contains('VUELO SANITARIO');
+        cy.get('span').contains('Traslado');
+        cy.get('small').contains('SIEN - SISTEMA INTEGRADO DE EMERGENCIA DE NEUQUEN');
+        cy.contains(' ACTUALIZAR ').click();
+        cy.plexTextArea('label="Observacion"', 'vuelo sanitario no disponible');
+        cy.plexButton("Guardar").click();
+        cy.wait('@updateHistorial').then((xhr) => {
+            expect(xhr.status).to.be.eq(200);
+            expect(xhr.response.body.paciente.documento).to.be.eq('2504196');
+        });
+
+        cy.login('30643636', 'asd', '5f68c547cbd0db303ac4aee9').then(t => {
+            token = t;
+            cy.goto('/com', token);
+        });
+
+        cy.get('plex-label').contains('Solicitante: PRUEBA, ALICIA').should('have.length', 1);
+        cy.get('plex-item').last().click();
+        cy.get('small').contains('VUELO SANITARIO');
+        cy.get('span').contains('Traslado');
+
+        cy.plexSelect('label="Nuevo estado"', 1).click();
+        cy.plexButton("Guardar").click();
+        cy.toast('success');
+        cy.plexSelectType('label="Estado"').click().get('.option').contains('HABILITADA').click();
+        cy.wait('@getDerivaciones').then((xhr) => {
+            expect(xhr.status).to.be.eq(200);
+        });
+        cy.get('plex-badge').contains('habilitada').should('have.length', 1);
+        cy.contains('habilitada').click({ force: true });
+        cy.get('plex-item').last().click();
+        cy.get('plex-options div div button').contains('HISTORIAL').click({ force: true });
+        cy.get('plex-panel').should('have.length', 3);
+        cy.get('.card').contains('Pasa a habilitada por');
+        cy.get('small').contains('Natalia Huenchuman de CENTRO OPERATIVO MEDICO');
+        cy.get('.card').contains('vuelo sanitario no disponible');
+        cy.get('plex-options div div button').contains('DERIVACIÓN').click({ force: true });
+        cy.plexSelectType('label="Nuevo estado"').click().get('.option').contains('asignada').click({ force: true });
+        cy.plexTextArea('label="Observacion"', 'derivación asignada');
+        cy.plexSelectType('label="Organización destino"').click().get('.option').contains('HOSPITAL AÑELO').click();
+
+        cy.plexButton("Guardar").click();
+        cy.toast('success');
+        cy.get('plex-tabs').contains('DERIVACIONES SOLICITADAS').click({ force: true });
+        cy.plexSelectType('label="Estado"').click().get('.option').contains('ASIGNADA').click();
+        cy.wait('@getDerivaciones').then((xhr) => {
+            expect(xhr.status).to.be.eq(200);
+        });
+        cy.get('plex-badge').contains('asignada').should('have.length', 1);
+        cy.login('30643636', 'asd', '5bae6b7b9677f95a425d9ee8').then(t => {
+            token = t;
+            cy.goto('/com', token);
+        });
+        cy.get('plex-label').contains('Solicitante: PRUEBA, ALICIA').should('have.length', 1);
+        cy.get('plex-item').last().click();
+        cy.plexSelectType('label="Nuevo estado"').click().get('.option').contains('rechazada').click();
+        cy.plexTextArea('label="Observacion"', 'derivación rechazada');
+        cy.plexButton("Guardar").click();
+        cy.toast('success');
+        cy.login('30643636', 'asd', '5f68c547cbd0db303ac4aee9').then(t => {
+            token = t;
+            cy.goto('/com', token);
+        });
+        cy.get('plex-tabs').contains('DERIVACIONES SOLICITADAS').click({ force: true });
+        cy.get('plex-badge').contains('rechazada').should('have.length', 1);
+        cy.get('plex-item').contains('rechazada').last().click();
+        cy.plexSelectType('label="Nuevo estado"').click().get('.option').contains('finalizada').click();
+        cy.plexTextArea('label="Observacion"', 'derivación finalizada');
+        cy.plexButton("Guardar").click();
+        cy.plexSelectType('label="Estado"').click().get('.option').contains('FINALIZADA').click();
+        cy.plexSelectType('label="Estado"').click().get('.option').contains('FINALIZADA').click();
+        cy.plexText('name="paciente"', '2504196');
+        cy.wait('@getDerivaciones').then((xhr) => {
+            expect(xhr.status).to.be.eq(200);
+        });
+        cy.get('plex-label').contains('Solicitante: PRUEBA, ALICIA').should('have.length', 1);
+        cy.get('plex-label').contains('Solicitante: PRUEBA, ALICIA').first().click({ force: true });
+        cy.get('span').contains('Traslado');
+        cy.get('plex-options div div button').contains('HISTORIAL').click({ force: true });
+        cy.get('plex-panel').should('have.length', 6);
     });
 });
