88065c72397a253f0a03f8b3b5d4a268d3eea723
Mariano Botta <marianoabotta@gmail.com>
2021-06-18 09:08:34 -0300

    feat(rup): elementos rup requeridos dinámicos (#366)

--- cypress/plugins/seed-services.js
+++ cypress/plugins/seed-services.js
@@ -6,6 +6,10 @@ module.exports.seedServices = async (mongoUri, params) => {
         const client = await connectToDB(mongoUri);
         const ServicesDB = await client.db().collection('andes-services');
 
+        await ServicesDB.deleteOne({ name: params.name });
+
+        convertDate(params);
+
         await ServicesDB.insertOne(params);
 
         return {};
@@ -14,4 +18,34 @@ module.exports.seedServices = async (mongoUri, params) => {
         console.log(e);
         return e;
     }
+}
+
+
+function convertDate(data) {
+    const dateISO = /\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(?:[.,]\d+)?Z/i;
+    const dateNet = /\/Date\((-?\d+)(?:-\d+)?\)\//i;
+    const traverse = function (o, func) {
+        for (let i of Object.keys(o)) {
+            o[i] = func.apply(this, [i, o[i]]);
+            if (o[i] !== null && typeof (o[i]) === 'object') {
+                traverse(o[i], func);
+            }
+        }
+    };
+    const replacer = function (key, value) {
+        if (typeof (value) === 'string') {
+            if (dateISO.test(value)) {
+                return new Date(value);
+            }
+            if (dateNet.test(value)) {
+                return new Date(parseInt(dateNet.exec(value)[1], 10));
+            }
+        }
+        return value;
+    };
+
+    if (data && typeof data === 'object') {
+        traverse(data, replacer);
+    }
+    return data;
 }
