7aa4336e2106069fe3e7e6df3d1de6e2db8290f4
Lautaro Molina <negro89_nqn@hotmail.com>
2021-09-02 15:45:45 -0300

    ref(tm): reemplaza table por plex-table

--- cypress/integration/core/tm/organizacion/organizacion.spec.ts
+++ cypress/integration/core/tm/organizacion/organizacion.spec.ts
@@ -14,15 +14,25 @@ context('Organizacion', () => {
         cy.server();
         cy.goto('/tm/organizacion', token);
         cy.route('PUT', '**/api/core/tm/organizaciones/**').as('putzonaSanitaria');
+        cy.route('GET', '**/api/core/tm/localidades**').as('localidades');
+        cy.route('GET', '**/api/core/tm/organizaciones?**').as('efector');
     })
 
     it('Seleccionar zona sanitaria', () => {
-        cy.get('table tbody tr').eq(0).plexButton('Editar').click();
-        cy.plexSelect('name="zonasSanitarias"', 0).click();
-        cy.plexButton('Guardar').click();
-        cy.swal('confirm', 'Los datos se actualizaron correctamente');
-        cy.wait('@putzonaSanitaria').then((xhr) => {
+        cy.plexText('name="nombre"', 'rendon');
+        cy.get('table tbody tr').eq(0).plexButton('Editar').click({ force: true });
+
+        cy.wait('@efector').then((xhr) => {
             expect(xhr.status).to.be.eq(200);
+        
+            cy.wait('@localidades').then((xhr) => {
+                expect(xhr.status).to.be.eq(200);
+            
+                cy.plexSelect('name="zonasSanitarias"', 0).click();
+            
+                cy.plexButton('Guardar').click();
+                cy.swal('confirm', 'Los datos se actualizaron correctamente');
+            });
         });
     })
 })
