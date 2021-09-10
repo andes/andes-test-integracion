098399841045679eba51ccd18058cd5d860ea6a6
martinebucarey <56874534+martinebucarey@users.noreply.github.com>
2021-09-10 12:06:51 -0300

    Seguimiento - Nueva forma de asignacion (#387)
    * fix(seguimiento): nueva forma de asignar
    
    * fix(seguimiento): paciente
    
    Co-authored-by: Pancho <martinebucarey@gmail,com>

--- cypress/integration/apps/epidemiologia/seguimiento/seguimiento.spec.js
+++ cypress/integration/apps/epidemiologia/seguimiento/seguimiento.spec.js
@@ -8,18 +8,21 @@ context('Seguimiento Epidemiológico', () => {
         cy.login('30643636', 'asd').then(t => {
             token = t;
         });
-        cy.task('database:create:paciente', {
-            template: 'validado', direccion: 'Irigoyen 1200'
-        }).then(p => {
-            validado = p;
-        });
-
+        cy.task('database:create:paciente',
+            {
+                _id: '5a2fe52ac439d943662d0a4c',
+                template: 'validado',
+                nombre: 'Prueba',
+                apellido: 'Seguimiento',
+                documento: 2006892
+            }).then(p => {
+                validado = p;
+            });
     })
-
 
     beforeEach(() => {
         cy.server();
-        cy.goto('/epidemiologia/ficha-epidemiologica', token);
+        cy.goto('/epidemiologia/seguimiento', token);
         cy.route('GET', '**/api/core/tm/organizaciones?**').as('organizaciones');
         cy.route('GET', '**api/core-v2/mpi/pacientes?**').as('busquedaPaciente');
         cy.route('POST', '**api/modules/forms/forms-epidemiologia/formEpidemiologia').as('registroFicha');
@@ -29,45 +32,13 @@ context('Seguimiento Epidemiológico', () => {
         cy.route('GET', '**/api/modules/seguimiento-paciente/seguimientoPaciente?**').as('buscarSeguimiento');
     })
 
-    it('crear nueva ficha covid19 caso confirmado - iniciar seguimiento', () => {
-        cy.plexText('name="buscador"', validado.documento);
-        cy.wait('@busquedaPaciente').then((xhr) => {
-            expect(xhr.status).to.be.eq(200);
-            expect(xhr.response.body[0].apellido).to.be.eq(validado.apellido);
-            expect(xhr.response.body[0].nombre).to.be.eq(validado.nombre);
-        });
-        cy.get('paciente-listado plex-item').contains(validado.apellido).click();
-        cy.plexDropdown('label="NUEVA FICHA"').click().get('a').contains('covid19').click();
-        cy.plexInputDinamico('phone', 'telefono', '{selectall}{backspace}22');
-        cy.plexSelectTypeDinamico('Clasificacion', 'Caso sospechoso{enter}');
-        cy.plexSelectTypeDinamico('tipo de busqueda', 'Activa{enter}');
-        cy.plexDateTimeDinamico('fecha de inicio de 1º síntoma', cy.today());
-        cy.plexSelectTypeDinamico('segunda clasificación', 'LAMP{enter}');
-        cy.plexSelectTypeDinamico('tipo de muestra', 'Aspirado{enter}');
-        cy.plexSelectTypeDinamico('LAMP (NeoKit)', 'Se detecta genoma de SARS-CoV-2{enter}');
-        cy.plexButton('Registrar ficha').click();
-        cy.wait('@registroFicha').then((xhr) => {
-            expect(xhr.status).to.be.eq(200);
-        });
-        cy.toast('success', 'Su ficha fue registrada correctamente');
-
-        cy.goto('/epidemiologia/seguimiento', token);
+    it('Iniciar seguimiento epidemiologico', () => {
         cy.plexButton('Buscar').click();
         cy.wait('@buscarSeguimiento').then((xhr) => {
             expect(xhr.status).to.be.eq(200);
-            expect(xhr.response.body.length).to.be.eq(0);
-
-        });
-        cy.plexSelectType('label="Organización"').clearSelect();
-        cy.plexSelectAsync('label="Organización"', 'CENTRO DE SALUD NUEVA ESPERANZA', '@organizaciones', 0);
-        cy.plexText('name="documento"', validado.documento);
-        cy.plexButton('Buscar').click();
-        cy.wait('@buscarSeguimiento');
-        cy.wait('@buscarSeguimiento').then((xhr) => {
-            expect(xhr.status).to.be.eq(200);
         });
 
-        cy.plexButton('Iniciar Seguimiento').click();
+        cy.plexButton('Iniciar').click();
         cy.wait('@postPrestacion').then((xhr) => {
             expect(xhr.status).to.be.eq(200);
         });
@@ -87,8 +58,8 @@ context('Seguimiento Epidemiológico', () => {
             expect(xhr.status).to.be.eq(200);
             expect(xhr.response.body.length).to.be.eq(0);
         })
-        cy.toast('success').click();
-        cy.plexButton('Validar consulta de seguimiento de paciente asociado a infección por COVID-19').click({ force: true });
+        cy.toast('success');
+        cy.plexButton('Validar consulta de seguimiento de paciente asociado a infección por COVID-19').click();
         cy.get('button').contains('CONFIRMAR').click();
 
         cy.wait('@patchPrestacion').then((xhr) => {
@@ -101,7 +72,6 @@ context('Seguimiento Epidemiológico', () => {
 
         cy.plexButton('SEGUIMIENTO').click();
 
-        cy.plexSelectType('label="Organización"').clearSelect();
         cy.plexSelectType('label="Estado"', 'Pendiente');
         cy.plexButton('Buscar').click();
         cy.wait('@buscarSeguimiento').then((xhr) => {
