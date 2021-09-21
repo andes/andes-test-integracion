eea4680a4cdb78b8695605ebcb4573a1e01f5c95
Andes <andesappsalud@gmail.com>
2021-09-21 14:45:44 -0300

    feat(mobile): comprueba que existe versión

---
+++ cypress/integration/apps/mobile/mobile-general.spec.ts
@@ -0,0 +1,17 @@
+context('mobile general', () => {
+    before(() => {
+    })
+
+    beforeEach(() => {
+        cy.server();
+        cy.viewport(550, 750);
+    });
+
+    it('Versión de app móvil visible', () => {
+        cy.goto('/mobile/')
+
+        cy.get('ion-menu-button').first().click();
+
+        cy.contains('Andes Neuquén. v').should('be.visible');
+    });
+});
