d577e779bf5cda6bb1577b57ca2661040082309e
plammel <lammelpablo@gmail.com>
2022-08-03 11:35:56 -0300

    MAT-95 (#425)
    * MAT-95
    
    * fix(inscripcion): test inscripcion profesional
    
    Co-authored-by: martinebucarey <martinebucarey@gmail.com>

--- cypress/e2e/apps/epidemiologia/inscripcion-profesionales.spec.ts
+++ cypress/e2e/apps/epidemiologia/inscripcion-profesionales.spec.ts
@@ -1,6 +1,6 @@
 /// <reference types="Cypress" />
 
-context('Inscripción profesionales COVID 19', () => {
+context('Creacion de usuarios para profesionales', () => {
     let token;
     before(() => {
         cy.seed();
@@ -18,8 +18,7 @@ context('Inscripción profesionales COVID 19', () => {
         cy.route('PUT', '**/api/core/tm/profesionales/actualizar').as('updateProfesional');
     })
 
-    it('Actualizar permisos para profesional con matricula vigente', () => {
-
+    it('Crear usuario para profesional', () => {
         cy.task('database:create:services', {
             "name": "renaper",
             "type": "static-client",
@@ -27,50 +26,20 @@ context('Inscripción profesionales COVID 19', () => {
                 "idTramite": 562207580
             }
         });
-
-
         cy.route('POST', '**/api/core/tm/profesionales/validar').as('pacienteValidado');
-
-
-        cy.plexText('name="documento"', '30643636');
-        cy.plexSelectType('label="Seleccione sexo"', 'femenino');
-        cy.plexInt('name="nroTramite"', '562207580');
-        cy.plexButton("VALIDAR").click();
-        cy.wait('@pacienteValidado').then((xhr) => {
-            expect(xhr.response.body.profesional.documento).to.be.eq('30643636');
-            expect(xhr.response.body.profesional.apellido).to.be.eq('Huenchuman');
-            expect(xhr.response.body.profesional.sexo).to.be.eq('femenino');
-        });
-        cy.plexBool('name="aceptaPermisos"', true);
-        cy.wait('@getUsuario').then((xhr) => {
-            expect(xhr.status).to.be.eq(200);
-            expect(xhr.response.body[0].usuario).to.be.eq(30643636);
-        });
-        cy.plexButton("Certificado de vacuna COVID19 ").click();
-        cy.wait('@updateProfesional').then((xhr) => {
-            expect(xhr.status).to.be.eq(200);
-            expect(xhr.response.body.documento).to.be.eq('30643636');
-        })
-    })
-
-    it('Crear usuario para profesional con matricula vigente', () => {
-
-        cy.route('POST', '**/api/core/tm/profesionales/validar').as('pacienteValidado2');
+        cy.plexText('name="nombre"', 'MARIA');
+        cy.plexText('name="apellido"', 'PEREZ');
         cy.plexText('name="documento"', '4466777');
         cy.plexSelectType('label="Seleccione sexo"', 'femenino');
-        cy.plexInt('name="nroTramite"', '562207580');
+        cy.plexDatetime('name="fechaNacimiento"', "01/01/1990");
         cy.plexButton("VALIDAR").click();
-        cy.wait('@pacienteValidado2').then((xhr) => {
+        cy.wait('@pacienteValidado').then((xhr) => {
             expect(xhr.response.body.profesional.documento).to.be.eq('4466777');
             expect(xhr.response.body.profesional.apellido).to.be.eq('PEREZ');
-            expect(xhr.response.body.profesional.sexo).to.be.eq('Femenino');
+            expect(xhr.response.body.profesional.sexo).to.be.eq('femenino');
         });
         cy.plexText('name="email"', 'prueba@gmail.com');
-        cy.plexBool('name="aceptaPermisos"', true);
-        cy.wait('@getUsuario').then((xhr) => {
-            expect(xhr.status).to.be.eq(200);
-            expect(xhr.response.body.length).to.be.eq(0);
-        });
+
         cy.plexButton(" Nuevo Usuario ").click();
         cy.wait('@createUser').then((xhr) => {
             expect(xhr.status).to.be.eq(200);
@@ -82,20 +51,4 @@ context('Inscripción profesionales COVID 19', () => {
             expect(xhr.response.body.documento).to.be.eq('4466777');
         })
     })
-
-    it('Validar profesional con matricula vencida', () => {
-        cy.route('POST', '**/api/core/tm/profesionales/validar').as('pacienteValidado3');
-        cy.plexText('name="documento"', '34934522');
-        cy.plexSelectType('label="Seleccione sexo"', 'masculino');
-        cy.plexInt('name="nroTramite"', '562207580');
-        cy.plexButton("VALIDAR").click();
-        cy.wait('@pacienteValidado3').then((xhr) => {
-            expect(xhr.response.body.profesional.documento).to.be.eq('34934522');
-            expect(xhr.response.body.profesional.apellido).to.be.eq('BOTTA');
-            expect(xhr.response.body.profesional.sexo).to.be.eq('masculino');
-        });
-        cy.get('plex-badge').contains(' Su matrícula no se encuentra vigente');
-        cy.plexButton("Renovar matrícula ");
-    })
-
 })
