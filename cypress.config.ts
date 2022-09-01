8e01dd8788f9c6af1780d2c4aa13adcf34d45cf7
Nicolás  Zabaleta <nzabaleta@hospitalneuquen.org.ar>
2022-09-01 11:47:46 -0300

    experimentalsession false

--- cypress.config.ts
+++ cypress.config.ts
@@ -31,8 +31,7 @@ export default defineConfig({
     setupNodeEvents(on, config) {
       return require('./cypress/plugins/index.js')(on, config)
     },
-    experimentalSessionAndOrigin: true,
-    testIsolation: legacy,
+    experimentalSessionAndOrigin: false,
     specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
   },
 })
