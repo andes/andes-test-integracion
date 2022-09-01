98364df9dcfd907467172a6e4c0eb0d506969714
Nicolás  Zabaleta <nzabaleta@hospitalneuquen.org.ar>
2022-09-01 11:38:15 -0300

    testisolation legacy

--- cypress.config.ts
+++ cypress.config.ts
@@ -32,6 +32,7 @@ export default defineConfig({
       return require('./cypress/plugins/index.js')(on, config)
     },
     experimentalSessionAndOrigin: true,
+    testIsolation: legacy,
     specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
   },
 })
