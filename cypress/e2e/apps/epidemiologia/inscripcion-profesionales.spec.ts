ef122d060a5bfb7dbfa48367453c947161083d6f
jiramirez_nqn@hotmail.com <jiramirez_nqn@hotmail.com>
2023-10-03 15:26:55 -0300

    (MAT-151) Pantalla creacion usuario

--- cypress/e2e/apps/epidemiologia/inscripcion-profesionales.spec.ts
+++ cypress/e2e/apps/epidemiologia/inscripcion-profesionales.spec.ts
@@ -2,6 +2,7 @@
 
 context('Creacion de usuarios para profesionales', () => {
     let token;
+    const documento = '325877758';
     before(() => {
         cy.seed();
         cy.login('30643636', 'asd').then(t => {
@@ -10,12 +11,11 @@ context('Creacion de usuarios para profesionales', () => {
     });
 
     beforeEach(() => {
-        cy.server();
         cy.goto('/inscripcion/profesionales', token);
-        cy.route('POST', '**api/modules/gestor-usuarios/usuarios/create').as('createUser');
-        cy.route('GET', '**/api/modules/gestor-usuarios/usuarios?**').as('getUsuario');
-        cy.route('PUT', '**/api/modules/gestor-usuarios/usuarios**').as('updateUsuario');
-        cy.route('PUT', '**/api/core/tm/profesionales/actualizar').as('updateProfesional');
+        cy.intercept('POST', '**api/modules/gestor-usuarios/usuarios/create').as('createUser');
+        cy.intercept('GET', '**/api/modules/gestor-usuarios/usuarios?**').as('getUsuario');
+        cy.intercept('PUT', '**/api/modules/gestor-usuarios/usuarios**').as('updateUsuario');
+        cy.intercept('PUT', '**/api/core/tm/profesionales/actualizar').as('updateProfesional');
     })
 
     it('Crear usuario para profesional', () => {
@@ -26,29 +26,29 @@ context('Creacion de usuarios para profesionales', () => {
                 "idTramite": 562207580
             }
         });
-        cy.route('POST', '**/api/core/tm/profesionales/validar').as('pacienteValidado');
+        cy.intercept('POST', '**/api/core/tm/profesionales/validar').as('pacienteValidado');
         cy.plexText('name="nombre"', 'TEST');
         cy.plexText('name="apellido"', 'PRUEBA');
-        cy.plexText('name="documento"', '325877758');
+        cy.plexInt('name="documento"', documento);
         cy.plexSelectType('label="Seleccione sexo"', 'masculino');
         cy.plexDatetime('name="fechaNacimiento"', "01/05/2019");
-        cy.plexButton("VALIDAR").click();
+        cy.plexButton("VALIDAR").click({ force: true });
         cy.wait('@pacienteValidado').then((xhr) => {
-            expect(xhr.response.body.profesional.documento).to.be.eq('325877758');
+            expect(xhr.response.body.profesional.documento).to.be.eq(documento);
             expect(xhr.response.body.profesional.apellido).to.be.eq('PRUEBA');
             expect(xhr.response.body.profesional.sexo).to.be.eq('masculino');
         });
         cy.plexText('name="email"', 'prueba@gmail.com');
 
         cy.plexButton(" Nuevo Usuario ").click();
-        cy.wait('@createUser').then((xhr) => {
-            expect(xhr.status).to.be.eq(200);
-            expect(xhr.response.body.documento).to.be.eq('325877758');
-            expect(xhr.response.body.organizaciones[0].id).to.be.eq('59380153db8e90fe4602ec02');
+        cy.wait('@createUser').then(({ response }) => {
+            expect(response.statusCode).to.be.eq(200);
+            expect(response.body.documento).to.be.eq(documento);
+            expect(response.body.organizaciones[0].id).to.be.eq('59380153db8e90fe4602ec02');
         })
-        cy.wait('@updateProfesional').then((xhr) => {
-            expect(xhr.status).to.be.eq(200);
-            expect(xhr.response.body.documento).to.be.eq('325877758');
+        cy.wait('@updateProfesional').then(({ response }) => {
+            expect(response.statusCode).to.be.eq(200);
+            expect(response.body.documento).to.be.eq(documento);
         })
     })
 })
